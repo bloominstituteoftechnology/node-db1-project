506042ea10139a5fca699f5fc0f5ae3d7553f624
Dexter Lewis <dexter.lewis@gmail.com>
2019-05-22 18:47:10 -0400

    MVP Complete

--- server.js
+++ server.js
@@ -1,7 +1,53 @@
 const express = require('express');
+const db = require('./data/accounts-model');
 
 const server = express();
+server.use(express.json());
 
 // your code here
+server.get('/', async (req, res) => {
+  try {
+    const accounts = await db.find();
+    res.status(200).json(accounts);
+  } catch (err) {
+    res.status(500).json({ error: 'Unable to process request' });
+  }
+});
+
+server.get('/:id', async (req, res) => {
+  try {
+    const account = await db.findById(req.params.id);
+    res.status(200).json(account);
+  } catch (err) {
+    res.status(500).json({ error: 'Unable to process request' });
+  }
+});
+
+server.post('/', async (req, res) => {
+  try {
+    const account = await db.add(req.body);
+    res.status(201).json(account);
+  } catch (err) {
+    res.status(500).json({ error: 'Unable to process request' });
+  }
+});
+
+server.delete('/:id', async (req, res) => {
+  try {
+    const account = await db.remove(req.params.id);
+    res.status(200).json(account);
+  } catch (err) {
+    res.status(500).json({ error: 'Unable to process request' });
+  }
+});
+
+server.put('/:id', async (req, res) => {
+  try {
+    const account = await db.update(req.params.id, req.body);
+    res.status(200).json(account);
+  } catch (err) {
+    res.status(500).json({ error: 'Unable to process request' });
+  }
+});
 
 module.exports = server;
