216d05341229ba3998292b3b58cf1048d80e159f
Sarahlee671 <sarah.lee1987@hotmail.com>
2019-06-03 13:44:45 -0700

    Added endpoints for get using methods find and findById

--- server.js
+++ server.js
@@ -1,7 +1,38 @@
 const express = require('express');
 
+const db = require('./data/accounts-model')
+
 const server = express();
 
+server.use(express.json());
+
+server.get('/api/budget', (req, res) => {
+    db
+        .find()
+        .then(rows => {
+            res.status(200).json({rows})
+        })
+        .catch(error => {
+            res.status(500).json({error: "Information could not be retrieved"})
+        })
+})
+
+server.get('/api/budget/:id', async (req, res) => {
+    try {
+        const account = await db.findById(req.params.id);
+        if (account.length===0) {
+            res.status(404).json({message: "The account with the specified ID does not exist"})
+        } else {
+            res.status(200).json(account);
+            
+        }
+    } catch (error) {
+        res.status(500).json({error: "The information could not be retrieved."})
+    }
+});
+
 // your code here
+
+
 
 module.exports = server;
