ed1573073e08f7174e823dba2a2bcc9f033a2b78
Sarahlee671 <sarah.lee1987@hotmail.com>
2019-06-03 14:18:47 -0700

    Added endpoints for post and delete

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const server = express();
 
 server.use(express.json());
 
-server.get('/api/budget', (req, res) => {
+server.get('/api/accounts', (req, res) => {
     db
         .find()
         .then(rows => {
@@ -17,21 +17,60 @@ server.get('/api/budget', (req, res) => {
         })
 })
 
-server.get('/api/budget/:id', async (req, res) => {
+server.get('/api/accounts/:id', async (req, res) => {
     try {
         const account = await db.findById(req.params.id);
         if (account.length===0) {
             res.status(404).json({message: "The account with the specified ID does not exist"})
         } else {
             res.status(200).json(account);
-            
         }
     } catch (error) {
         res.status(500).json({error: "The information could not be retrieved."})
     }
 });
 
-// your code here
+server.post('/api/accounts', (req, res) => {
+    const {name, budget} = req.body;
+    if (!name || !budget) {
+        res.send(400).json({ message: "Please provide name and budget for the account"})
+    }
+    db
+        .add({
+            name,
+            budget
+        })
+        .then(account => {
+            res.status(201).json(account);
+        })
+        .catch(error => {
+            res.status(500).json({ error: "There was an error while saving account to the database"})
+        })
+})
+
+server.delete('/api/accounts/:id', (req, res) => {
+    const id = req.params.id
+    db
+        .remove(id)
+        .then(id => {
+            if (id > 0) {
+                res.status(200).json({message:`${id} record(s) has/have successfully been deleted`})
+                
+            } else {
+                res.status(404).json({ message: "The account with the specified ID does not exist."})
+                
+            }
+        })
+        .catch(error => {
+            res.status(500).json({error: "The account could not be removed"})
+        })
+})
+
+
+
+
+
+
 
 
 
