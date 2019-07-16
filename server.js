c429d8308a87bf57c531b5ccc46b7c4ca42ae26d
Megan <megan.miller@smoldev.dev>
2019-07-16 10:41:27 -0500

    get and put added
    MVP not met

--- server.js
+++ server.js
@@ -6,4 +6,31 @@ const server = express();
 
 server.use(express.json());
 
+server.get('/', (req, res) => {
+    db('accounts')
+    .then(accounts => {
+        res.status(200).json(accounts)
+    })
+    .catch(error => {
+        res.status(500).json(error);
+    })
+});
+
+server.get('/:id', (req, res) => {
+    // code
+})
+
+server.post('/', (req, res) => {
+    const account = req.body;
+
+    db('accounts').insert(account, 'id')
+    .then(accounts => {
+        const accountbyID = accounts[0];
+        res.status(201).json(accountbyID)
+    })
+    .catch(error => {
+        res.status(500).json(error)
+    })
+})
+
 module.exports = server;
