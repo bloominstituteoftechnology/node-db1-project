053e1b1ea9c4d6d72489e5f17a55110c58a3c94d
Scott Schindler <scottschindler29@gmail.com>
2019-08-07 23:27:08 -0400

    first changes

--- knexfile.js
+++ knexfile.js
@@ -1,15 +1,17 @@
 module.exports = {
   development: {
-    client: 'sqlite3',
+    client: "sqlite3",
     connection: {
-      filename: './data/budget.db3',
+      filename: "./data/budget.db3"
     },
     useNullAsDefault: true,
     migrations: {
-      directory: './data/migrations'
+      directory: "./data/migrations"
     },
     seeds: {
-      directory: './data/seeds'
+      directory: "./data/seeds"
     }
-  },
+  }
 };
+
+//change me
