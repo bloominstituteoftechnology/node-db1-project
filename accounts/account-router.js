8529bb1166bee14b6f81d2ded6a1a2a574614dc4
Ken Ruf <ruf.kenichi@gmail.com>
2019-08-07 20:50:35 -0500

    added validateAccountId middleware

--- accounts/account-router.js
+++ accounts/account-router.js
@@ -6,7 +6,8 @@ const router = express.Router();
 
 router.get('/', async (req, res) => {
   try {
-
+    const accounts = await db.select('*').from('accounts')
+    res.status(200).json(accounts)
   }
   catch(error) {
     res.status(500).json({ message: "Could Not Get Accounts", error: error})
@@ -14,6 +15,7 @@ router.get('/', async (req, res) => {
 });
 
 router.get('/:id', async (req, res) => {
+  const { id } = req.params;
   try {
 
   }
@@ -42,11 +44,28 @@ router.put('/:id', async (req, res) => {
 
 router.delete('/:id', async (req, res) => {
   try {
-    
+
   }
   catch(error) {
     res.status(500).json({ message: "Could Not Delete Account", error: error})
   }
 })
+
+// middlewares
+
+async function validateAccountId( req, res, next ) {
+  const { id } = req.params;
+  try {
+    const account = await db('accounts').where('id' = id);
+    if(account) {
+      next();
+    } else(error) {
+      res.status(404).json({ message: `Account with id ${id} does not exist`, error: error})
+    }
+  }
+  catch(error) {
+    res.status(500).json({ message: "Error with validateAccountId", error: error})
+  }
+}
 
 module.exports = router;
