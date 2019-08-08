0f24868333ed2b2c2674ad5e34db300f82dc6e5e
Poly Goveia <poly.goveia@gmail.com>
2019-08-07 20:18:30 -0700

    add get all

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
-const express = require('express');
+const express = require("express");
 
-const db = require('./data/dbConfig.js');
+const db = require("./data/dbConfig.js");
 
 const server = express();
 
 server.use(express.json());
+
+// GET ALL
+server.get("/", async (req, res) => {
+  try {
+    const accounts = await db("accounts");
+    res.status(200).json(accounts);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// GET ID
+
+// POST
+
+// PUT
+
+// DELETE
 
 module.exports = server;
