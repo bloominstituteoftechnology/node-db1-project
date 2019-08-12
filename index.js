00047b92e1f0936b7e9356b2674e1fc2fd4e68af
andrew dillon <majik895@gmail.com>
2019-08-12 19:00:30 -0400

    added .delete

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const server = require('./server.js');
 const PORT = process.env.PORT || 4000;
 
 server.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}...`);
+  console.log(`Kicking back and having a cold one on port ${PORT}...`);
 });
--- server.js
+++ server.js
@@ -16,7 +16,7 @@ server.get('/api/accounts', (req, res) => {
             res.status(200).json(item)
         })
         .catch(error => {
-            req.status(500).json({ message: "There was a database error." })
+            req.status(500).json({ tidings: "E.R.R.O.R Database Destroyed" })
         })
 });
 
@@ -28,8 +28,55 @@ server.get('/api/accounts/:id', (req, res) => {
             res.status(200).json(account)
         })
         .catch(error => {
-            res.status(500).json({ message: "There was an error getting that ID." })
+            res.status(500).json({ tidings: "There was an E.R.R.O.R with your request but dont worry youre not as dumb as you look." })
         });
 })
+
+server.post('/api/accounts', (req, res) => {
+    const account = req.body;
+    if (!account.name || !account.budget) {
+        res.status(400).json({ tidings: "You can't do that! Add a name and budget." })
+    } else {
+        db("accounts")
+            .insert(account)
+            .then(account => {
+                res.status(200).json(account)
+            })
+            .catch(error => {
+                res.status(500).json({ tidings: "E.R.R.O.R. adding the Data!" })
+            })
+    }
+});
+
+server.put('/api/accounts/:id', (req, res) => {
+    const changes = req.body;
+    db("accounts")
+        .where({ id: req.params.id })
+        .update(changes)
+        .then(count => {
+            if (count > 0) {
+                res.status(200).json(count);
+            } else {
+                res.status(404).json({ tidings: 'Oh snap we cant find that id.' });
+            }
+        })
+        .catch(error => {
+            res.status(500).json({ tidings: 'E.R.R.O.R. while trying to update that account.' });
+        });
+});
+
+server.delete('/api/accounts/:id', (req, res) => {
+    db("accounts")
+        .where({ id: req.params.id })
+        .del()
+        .then(count => {
+            count > 0 ? res.status(200).json(count) : res.status(400).json({ tidings: "Oops couldn't locate that account. Try again." })
+        })
+        .catch(error => {
+            res.status(500).json({ tidings: "Big fat E.R.R.O.R. while trying to delete that account." })
+        })
+});
+
+
 
 module.exports = server
