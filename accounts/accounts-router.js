2d5545c45c0476d31f07e1a66e4e3c95c31a0b88
Zaur <42130952+ZaurInvictus@users.noreply.github.com>
2019-08-20 13:02:24 -0500

    PUT and DELETE handlers created

--- accounts/accounts-router.js
+++ accounts/accounts-router.js
@@ -39,7 +39,45 @@ router.post('/', (req, res) => {
 })
 
 
+// PUT
+router.put('/:id', (req, res) => {
+  const changes = req.body
+  
+  db('accounts')
+  .where('id', '=', req.params.id)
+  .update(changes)
 
+  .then(count => {
+    if(count > 0) {
+      res.status(200).json(count)
+    } else {
+      res.status(404).json({ message: 'Id is not found' })
+    }
+  })
+  .catch(error => {
+    res.status(500).json({message: 'error updating'})
+  })
+})
+
+
+// DELETE
+router.delete('/:id', (req, res) => {
+
+ db('accounts')
+ .where('id', '=', req.params.id)
+ .del()
+
+ .then(count => {
+    if(count > 0) {
+      res.status(200).json(count)
+    } else {
+      res.status(404).json({message: 'Id is not fount'})
+    }
+ })
+ .catch(error => {
+   res.status(500).json({message: 'error deleting'})
+ })
+})
 
 
 
