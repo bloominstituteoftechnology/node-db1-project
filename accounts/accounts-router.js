f8509de118316f6083a0fc03a5ee2b3182c84321
Zaur <42130952+ZaurInvictus@users.noreply.github.com>
2019-08-20 13:01:28 -0500

    POST handler for account created

--- accounts/accounts-router.js
+++ accounts/accounts-router.js
@@ -6,7 +6,6 @@ const router = express.Router()
 
 
 // GET
-
 router.get('/', (req, res) => {
   db('accounts')
     .then(accounts => {
@@ -16,6 +15,33 @@ router.get('/', (req, res) => {
       res.status(500).json({message: 'error getting accounts from db'})
     })
 })
+
+
+
+// POST
+router.post('/', (req, res) => {
+  const { name, budget } = req.body
+
+  if(!name || !budget) {
+    res.status(400).json({
+      message: 'Please provide name and budget'
+    })
+   return
+  }
+
+  db('accounts').insert(req.body, 'id')
+  .then(count => {
+     res.status(200).json(count)
+  })
+  .catch(error => {
+    res.status(500).json({message: 'Internal server error'})
+  })
+})
+
+
+
+
+
 
 
 module.exports = router
