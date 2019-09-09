3838af5d0747ab24c9bac7d3a25e32897614e1a7
justinelai <justine.lai@gmail.com>
2019-09-09 14:47:23 -0400

    seed data, get all working

--- server.js
+++ server.js
@@ -6,4 +6,32 @@ const server = express();
 
 server.use(express.json());
 
+server.get('/api', (req, res) => {
+    db('accounts')
+    .then(accounts => res.status(200).json(accounts))
+    .catch(err => res.status(500).json({ message: 'Failed to get accounts' }))
+});
+
+server.get('/api/:id', (req, res) => {
+    const {id} = req.params;
+    db('accounts')
+        .where({id})
+        .first()
+        .then(accts => res.status(200).json(accts))
+        .catch(err => res.status(500).json({ message: 'Failed to get account' }))
+
+});
+
+server.post('/api', (req, res) => {
+
+});
+
+server.put('/api/:id', (req, res) => { 
+
+});
+
+server.delete('/api/:id', (req, res) => {
+
+});
+
 module.exports = server;
