4ce1a033fcad1c54f98e353d0134cbee2a30e4f1
eralpkor <eralp.ny@gmail.com>
2019-10-07 19:42:23 -0400

    added put, delete

--- accounts/accounts-router.js
+++ accounts/accounts-router.js
@@ -48,7 +48,32 @@ router.post('/', validateAccount, (req, res) => {
 
 // PUT api/accounts/:id to update an account
 router.put('/:id', (req, res) => {
-  
+  const { id } = req.params;
+  db('accounts')
+    .where({ id: id})
+    .update(req.body)
+    .then(count => {
+      count ? res.status(200).json({ message: 'Account has been updated...'}) : res.status(404).json({ message: `Cannot get ID ${id}`})
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: 'error updating account...'})
+    });
+});
+
+// Delete /api/accounts delete an account...
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  db('accounts')
+    .where({ id: id})
+    .del()
+    .then(count => {
+      count ? res.status(200).json({ message: `Account with ID: ${id} deleted`}) : res.status(404).json({ message: 'Invalid account ID...'})
+    })
+    .catch(err => { 
+      console.log(err);
+      res.status(500).json({ error: 'error deleting...'});
+    });
 });
 
 // Custom Middleware
