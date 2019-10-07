58f9d07853613dd4c409e0dd9c66959a82202d97
noahfranco <noahfra@gmail.com>
2019-10-07 17:11:55 -0400

    finsihed creating routers

--- router.js
+++ router.js
@@ -4,3 +4,23 @@ const db = require("./data/dbConfig.js");
 
 const router = express.Router(); 
 
+router.get("/", (req, res) => {
+    
+}); 
+
+router.post("/", (req, res) => {
+
+});
+
+router.get("/:id", (req, res) => {
+    
+}); 
+
+router.put("/:id", (req, res) => {
+
+}); 
+
+router.delete("/:id", (req, res) => {
+    
+})
+
