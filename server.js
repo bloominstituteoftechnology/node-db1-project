ad86bcf1480e2bbb2e295ec37332780c7d947975
MicahJank <MicahJank@users.noreply.github.com>
2019-11-13 17:05:28 -0600

    get request to the database is working

--- server.js
+++ server.js
@@ -6,4 +6,15 @@ const server = express();
 
 server.use(express.json());
 
+
+server.get('/', (req, res) => {
+    db('accounts')
+        .then(accounts => {
+            res.status(200).json(accounts);
+        })
+        .catch(err => {
+            res.status(500).json({ message: 'Unable to retrieve the accounts from the database' });
+        });
+});
+
 module.exports = server;
