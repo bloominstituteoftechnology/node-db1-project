4dd430b4148caf05b6622a919806e59ed3187516
Carlos Mitchell <kakismitchell@gmail.com>
2019-11-13 23:06:03 -0500

    MVP

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ const express = require('express');
 
 const db = require('./data/dbConfig.js');
 
+const dataRouters = require('./data-Routers/Router');
+
 const server = express();
 
 server.use(express.json());
+
+server.use('/api/accounts', dataRouters);
 
 module.exports = server;
