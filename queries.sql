21e6d9c0dcf77b9b9fd22a3b02f7fc13fbf59579
Brandon Davis <49970067+Bdavis90@users.noreply.github.com>
2019-11-13 20:24:36 -0500

    MVP

--- queries.sql
+++ queries.sql
@@ -1,16 +1,24 @@
 -- Database Queries
 
 -- Find all customers with postal code 1010
+SELECT * FROM Customers where postalcode=1010 limit 3
 
 -- Find the phone number for the supplier with the id 11
+SELECT phone FROM Suppliers where supplierid=11
 
 -- List first 10 orders placed, sorted descending by the order date
+SELECT * FROM Orders order by orderdate desc limit 10
 
 -- Find all customers that live in London, Madrid, or Brazil
+SELECT * FROM [Customers] where city="Berlin" or city="London" or city="Madrid" or city="Brazil"
+or
+SELECT * FROM [Customers] where city in ('Berlin', 'London', 'Madrid', 'Brazil')
 
 -- Add a customer record for "The Shire", the contact name is "Bilbo Baggins" the address is -"1 Hobbit-Hole" in "Bag End", postal code "111" and the country is "Middle Earth"
+insert into customers (customername, contactname, address, city, postalcode, country) values('The Shire', 'Bilbo Baggins', '1 Hobbit-Hole', 'Bag End', '111', 'Middle Earth')
 
 -- Update Bilbo Baggins record so that the postal code changes to "11122"
+UPDATE customers set postalcode='11122' where customerid=92
 
 -- (Stretch) Find a query to discover how many different cities are stored in the Customers table. Repeats should not be double counted
 
--- server.js
+++ server.js
@@ -1,9 +1,55 @@
-const express = require('express');
+const express = require("express");
 
-const db = require('./data/dbConfig.js');
+const db = require("./data/dbConfig.js");
 
 const server = express();
 
 server.use(express.json());
 
+server.get("/account", (req, res) => {
+  db("accounts")
+    .then(account => res.status(200).json(account))
+    .catch(err =>
+      res.status(500).json({ message: "Could not retrieve account" })
+    );
+});
+
+server.get("/account/:id", (req, res) => {
+  const id = req.params.id;
+  db.select("*")
+    .from("accounts")
+    .where({ id })
+    .then(account => res.status(200).json(account));
+});
+
+server.post("/account", (req, res) => {
+  accountBody = req.body;
+  db.insert(accountBody)
+    .into("accounts")
+    .then(account => res.status(201).json(account))
+    .catch(err =>
+      res.status(500).json({ message: "Could not add new account" })
+    );
+});
+
+server.put("/account/:id", (req, res) => {
+  const id = req.params.id;
+  const changes = req.body;
+  db("accounts")
+    .where({ id })
+    .update(changes)
+    .then(account => res.status(200).json(account))
+    .catch(err =>
+      res.status(500).json({ message: "Could not update account" })
+    );
+});
+
+server.delete("/account/:id", (req, res) => {
+  const id = req.params.id;
+  db("accounts")
+    .where({ id })
+    .del()
+    .then(account => res.status(200).json({ message: "Account was deleted" }))
+    .catch(err => res.status(500).json({ message: "Count not delete accout" }));
+});
 module.exports = server;
