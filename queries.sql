d64e43800215ea58c1fa081677b543bb41b8ccfe
spettigrew <sara.pettigrew5@gmail.com>
2019-12-18 12:56:49 -0700

    SQL Commands added

--- queries.sql
+++ queries.sql
@@ -38,15 +38,54 @@ LIMIT 10;
 
 -- Find all customers that live in London, Madrid, or Brazil
 
--- SELECT City
--- FROM Customers
--- City 'London', 'Madrid', 'Brazil'
+SELECT City, Country
+FROM Customer
+WHERE City = 'London'
+    OR City = 'Madrid'
+    OR Country = 'Brazil';
+-- Answer: London	UK
+-- Madrid	Spain
+-- London	UK
+-- Sao Paulo	Brazil
+-- London	UK
+-- London	UK
+-- Sao Paulo	Brazil
+-- Madrid	Spain
+-- Campinas	Brazil
+-- Rio de Janeiro	Brazil
+-- London	UK
+-- Rio de Janeiro	Brazil
+-- Sao Paulo	Brazil
+-- Rio de Janeiro	Brazil
+-- Madrid	Spain
+-- London	UK
+-- Sao Paulo	Brazil
+-- Resende	Brazil
 
 
 -- Add a customer record for "The Shire", the contact name is "Bilbo Baggins" the address is -"1 Hobbit-Hole" in "Bag End", postal code "111" and the country is "Middle Earth"
+INSERT INTO Customer
+    (CompanyName, ContactName, Address, City, PostalCode, Country)
+VALUES
+    ('The Shire', 'Bilbo Baggins', '1 Hobbit-Hole', 'Bag End', 111, 'Middle Earth');
+
+-- Answer: 
+-- The Shire Bilbo Baggins 1 Hobbit-Hole Bag End 111 Middle Earth	
+
 
 -- Update Bilbo Baggins record so that the postal code changes to "11122"
+UPDATE Customer 
+SET PostalCode= 11122
+WHERE CompanyName = 'The Shire';
+
+-- Answer: 
+-- PostalCode = 11122
 
 -- (Stretch) Find a query to discover how many different cities are stored in the Customers table. Repeats should not be double counted
 
--- (Stretch) Find all suppliers who have names longer than 20 characters. You can use `length(SupplierName)` to get the length of the name
+-- (Stretch) Find all suppliers who have names longer than 20 characters. You can use `length(SupplierName)` to get the length of the name 
+--name.length >=20
+
+
+-- Names in CAPS are SQL commands, names in lower case are columns and table names, names in single quotes are values. 
+-- SUM will add all the numbers in the column requested.
