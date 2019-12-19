bbae634d994117d5f4deea479e345ec0deb70060
spettigrew <sara.pettigrew5@gmail.com>
2019-12-19 16:59:14 -0700

    Import server.use router

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const express = require('express');
-
-const db = require('./data/dbConfig.js');
+const accountRouter = require('./accounts/account-router')
 
 const server = express();
 
 server.use(express.json());
+server.use('/api/accounts', accountRouter) //import router into server.
+
+server.get('/', (req, res) => {
+    return res.send('<h2> I am in the server.</h2>')
+})
 
 server.use((err, req, res, next) => {
     console.log(err)
     return res.status(500).json({ message: 'Something is wrong' })
 })
 
-module.exports = server;
+module.exports = server; 
+
+// error 404 means to check your server.use /api/ router
