a5112d073217fe538e5a3311a8bdc5c4ad1d8bf0
spettigrew <sara.pettigrew5@gmail.com>
2019-12-18 18:08:02 -0700

    API endpoints setup

--- accounts/account-router.js
+++ accounts/account-router.js
@@ -7,7 +7,7 @@ const router = express.Router()
 
 router.get('/', async (req, res, next) => {
     try {
-       return res.json(await db.select('*').from('accounts'))
+       return res.json(await db('accounts').select())
     }
     catch (err) {
         next(err)
@@ -16,34 +16,48 @@ router.get('/', async (req, res, next) => {
 
 router.get('/:id', async (req, res, next) => {
     try {
-        return res.json(await db('accounts').where('id', req.params.id).first())k
+        return res.json(await db('accounts').where('id', req.params.id).first())
     }
     catch(err) {
     next(err)
     }
 })
 
+router.post('/', async (req, res, next) => {
+    try {
+        const payload = {
+            name: req.body.name,
+            budget: req.body.budget,
+        }
+
+        const [id] = await db('accounts').insert(payload)
+        return res.json(await db('accounts').where('id', id).first())
+    }
+    catch (err) {
+        next(err)
+    }
+})
+
 router.put('/:id', async (req, res, next) => {
     try {
+        const payload = {
+            name: req.body.name,
+            budget: req.body.budget,
+        }
 
+        await db('accounts').where('id', req.params.id).update(payload)
+        return res.json(await db('accounts').where('id', req.params.id).first())
     }
     catch (err) {
         next(err)
     }
 })
 
-router.post('/:id', async (req, res, next) => {
-    try {
-
-    }
-    catch (err) {
-        next(err)
-    }
-})
 
 router.delete('/:id', async (req, res, next) => {
     try {
-
+        await db('accounts').where('id', req.params.id).del()
+        return res.status(204).end()
     }
     catch (err) {
         next(err)
