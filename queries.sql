9c4cb544f1a8c6e0c7caf15bc948ae51848bc6b6
newbi462 <isthisemailfree@gmail.com>
2020-01-19 11:01:46 -0500

    -- Find all customers that live in London, Madrid, or Brazil

--- queries.sql
+++ queries.sql
@@ -12,9 +12,11 @@ WHERE SupplierID=11;
 SELECT * FROM Orders
 WHERE OrderID<=10257
 ORDER BY OrderDate DESC;
---NOT HAPPY WITH THE "WHERE" use, look for bettwe way. 
+--NOT HAPPY WITH THE "WHERE" use, look for bettwe way.
 
 -- Find all customers that live in London, Madrid, or Brazil
+SELECT * FROM Customers
+WHERE City="London" OR City="Madrid" OR Country="Brazil";
 
 -- Add a customer record for "The Shire", the contact name is "Bilbo Baggins" the address is -"1 Hobbit-Hole" in "Bag End", postal code "111" and the country is "Middle Earth"
 
