67977c61febd96cba99a856e80e99417ef23ae15
newbi462 <isthisemailfree@gmail.com>
2020-01-19 11:52:40 -0500

    -- Update Bilbo Baggins record so that the postal code changes to "11122"

--- queries.sql
+++ queries.sql
@@ -31,6 +31,8 @@ VALUES ("Middle Earth", "The Shire", "Bilbo Baggins", "1 Hobbit-Hole", "Bag End"
 
 
 -- Update Bilbo Baggins record so that the postal code changes to "11122"
+UPDATE Customers SET PostalCode = 11122 where CustomerID=92;
+
 
 -- (Stretch) Find a query to discover how many different cities are stored in the Customers table. Repeats should not be double counted
 
