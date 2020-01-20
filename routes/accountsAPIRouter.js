52aafac0e3cd84d75e4102a96a1346d52207981e
newbi462 <isthisemailfree@gmail.com>
2020-01-19 23:28:12 -0500

    GET | CRUD

--- routes/accountsAPIRouter.js
+++ routes/accountsAPIRouter.js
@@ -4,7 +4,21 @@ const db = require('./../data/dbConfig.js');
 
 const router = express.Router();
 
-
+router.get('/', (request, responce) => {
+  db.select('*').from("accounts")
+    .then(acc => {
+      responce.json(acc);
+    })
+    .catch( error => {
+      console.log(error);
+      responce.status(500).json(
+        {
+          error: "Get Failed."
+        }
+      )
+    })
+});// returns an empy arr?
+// npx knex seed:run
 
 
 module.exports = router;
