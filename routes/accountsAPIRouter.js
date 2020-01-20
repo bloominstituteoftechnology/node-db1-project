94250479bfbf38ab186620f09903fae8ee4496bc
newbi462 <isthisemailfree@gmail.com>
2020-01-20 12:03:48 -0500

    POST of CRUD, resolves to new account

--- routes/accountsAPIRouter.js
+++ routes/accountsAPIRouter.js
@@ -4,6 +4,7 @@ const db = require('./../data/dbConfig.js');
 
 const router = express.Router();
 
+//GET
 router.get('/', (request, responce) => {
   db.select('*').from("accounts")
     .then(acc => {
@@ -20,5 +21,47 @@ router.get('/', (request, responce) => {
 });// returns an empy arr?
 // npx knex seed:run
 
+//POST
+router.post('/', (request, responce) => {
+  //console.log(request.body);
+  //db.select('*').from("accounts").insert(request.body)
+  db("accounts").insert(request.body)
+    .then(idOfAdded => {
+      //console.log(idOfAdded);// id = index 0
+      db("accounts").where({id:idOfAdded[0]})
+      .then(acc => {
+        responce.json(acc);
+      })
+      .catch( error => {
+        console.log(error);
+        responce.status(500).json(
+          {
+            error: "Where by ID Failed."
+          }
+        )
+      })
+      //responce.json(output);
+      //responce.json(idOfAdded);
+    })
+    .catch( error => {
+      console.log(error);
+      responce.status(500).json(
+        {
+          error: "Post Failed."
+        }
+      )
+    })
+});
+
+//PUT
+
+//DEL
+
 
 module.exports = router;
+
+/*
+npx knex migrate:rollback
+npx knex migrate:latest
+npx knex seed:run
+*/
