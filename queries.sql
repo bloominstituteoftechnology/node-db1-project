513f4b9ae287a8d385d2bee6ea6423cb18bf4dce
Joseph Graham <Joseph@Chasingthefiks.net>
2020-01-21 13:08:46 -0700

    initial commit - SQL querys written. need insert/update

--- queries.sql
+++ queries.sql
@@ -8,11 +8,11 @@ Select phoneNumber from suppliers where id =11
 -- List first 10 orders placed, sorted descending by the order date
 Select Top(10) orders sorted by orderdate desc
 -- Find all customers that live in London, Madrid, or Brazil
-Select * from suctomers where country in ('London', 'Madrid', 'Brazil')
+Select * from customers where country in ('London', 'Madrid', 'Brazil')
 -- Add a customer record for "The Shire", the contact name is "Bilbo Baggins" the address is -"1 Hobbit-Hole" in "Bag End", postal code "111" and the country is "Middle Earth"
 insert into customer 
 -- Update Bilbo Baggins record so that the postal code changes to "11122"
-
+update 
 -- (Stretch) Find a query to discover how many different cities are stored in the Customers table. Repeats should not be double counted
 select count(cities) from customers group by city order by city
 -- (Stretch) Find all suppliers who have names longer than 20 characters. You can use `length(SupplierName)` to get the length of the name
