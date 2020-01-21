157a9cff360b4d87ce0173c4222a338ef9e4a13b
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-01-20 22:33:23 -0500

    update /server.js

--- server.js
+++ server.js
@@ -1,9 +1,48 @@
-const express = require ('express')
+/***********************************************************
+  server
+***********************************************************/
 
-const db = require ('./data/dbConfig.js')
+/// tools ///
+const express = require ('express')
+const helmet = require ('helmet')
+const morgan = require ('morgan')
+
+const {
+  respondWithError,
+} = require ('./middleware')
+
+/***************************************
+  setup server
+***************************************/
 
 const server = express ()
 
-server.use (express.json ())
+/// wares ///
+server.use ([
+  helmet (),
+  morgan ('dev'),
+])
+
+/// routers ///
+const subs = [ 'api' ]
+subs.forEach ((sub) => {
+  server.use (
+    `/${sub}`,
+    require (`./routers/${sub}`).router,
+  )
+})
+
+/// requests ///
+server.route ('/')
+  .get ((ri, ro) => {
+    ro
+      .status (200)
+      .send (`<h1>web-db-1</h1>`)
+  })
+
+server.route ('*')
+  .all (respondWithError (501))
+
+/**************************************/
 
 module.exports = server
