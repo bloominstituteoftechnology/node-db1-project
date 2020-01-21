ba5041f5446d350b65e8667d53e8652e34c93987
Chelsea Wetzel <chelseagilmore14@gmail.com>
2020-01-20 20:50:22 -0500

    all CRUD in knex

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const server = require('./server.js');
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 6000;
 
 server.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
+});
+
+server.use((err, req, res, next) => {
+  console.log(err)
+  res.status(500).json({
+    message: "something went wrong"
+  });
 });
