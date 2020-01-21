e9b1571797eb75fbf0949841b3452137ce2ef8d6
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-01-20 20:23:12 -0500

    number queries

--- queries.sql
+++ queries.sql
@@ -1,16 +1,16 @@
 -- # Database Queries
 
--- Find all customers with postal code 1010
+-- 1. Find all customers with postal code 1010
 SELECT *
 FROM [Customers]
 WHERE [PostalCode] = 1010
 
--- Find the phone number for the supplier with the id 11
+-- 2. Find the phone number for the supplier with the id 11
 SELECT [Phone]
 FROM [Suppliers]
 WHERE [SupplierID] = 11
 
--- List first 10 orders placed, sorted descending by the order date
+-- 3. List first 10 orders placed, sorted descending by the order date
 SELECT *
 FROM (
   -- earliest (first) 10 orders, [OrderDate] asc
@@ -21,12 +21,12 @@ FROM (
 )
 ORDER BY [OrderDate] DESC -- now [OrderDate] desc
 
--- Find all customers that live in London, Madrid, or Brazil
+-- 4. Find all customers that live in London, Madrid, or Brazil
 
--- Add a customer record for "The Shire", the contact name is "Bilbo Baggins" the address is -"1 Hobbit-Hole" in "Bag End", postal code "111" and the country is "Middle Earth"
+-- 5. Add a customer record for "The Shire", the contact name is "Bilbo Baggins" the address is -"1 Hobbit-Hole" in "Bag End", postal code "111" and the country is "Middle Earth"
 
--- Update Bilbo Baggins record so that the postal code changes to "11122"
+-- 6. Update Bilbo Baggins record so that the postal code changes to "11122"
 
--- (Stretch) Find a query to discover how many different cities are stored in the Customers table. Repeats should not be double counted
+-- 7. (Stretch) Find a query to discover how many different cities are stored in the Customers table. Repeats should not be double counted
 
--- (Stretch) Find all suppliers who have names longer than 20 characters. You can use `length(SupplierName)` to get the length of the name
+-- 8. (Stretch) Find all suppliers who have names longer than 20 characters. You can use `length(SupplierName)` to get the length of the name
