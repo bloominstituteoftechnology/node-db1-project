0a54cdab50a54303bcec8a7fbab5aaeb96a7ef10
lukasjaronis <jaronisluke13@gmail.com>
2020-01-21 19:17:09 -0500

    .get /:id done

--- data/Accounts/Router.js
+++ data/Accounts/Router.js
@@ -18,3 +18,20 @@ router.get('/', (request, response) => {
     })
 })
 
+
+router.get('/:id', (request, response) => {
+    knex
+    .select('*')
+    .from('accounts')
+    .where({id: request.params.id})
+    .first() // accounts[0]
+    .then(post => {
+        response.status(200).json(post)
+    })
+    .catch(error => {
+        response.status(500).json({
+            errorMsg: `Error retrieving the account. ${error}`
+        })
+    })
+})
+
