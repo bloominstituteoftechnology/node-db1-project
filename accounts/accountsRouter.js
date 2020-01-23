9f41695f44f49a16976f86feca1623e16b0a4465
imxande <alerodriguezlarosa@gmail.com>
2020-01-23 03:04:20 -0500

    creating an account works, and tested

--- accounts/accountsRouter.js
+++ accounts/accountsRouter.js
@@ -19,13 +19,34 @@ router.get('/', async (req, res, next) =>{
 // I need to get account by its respective ID
 router.get('/:id', async (req, res, next) =>{
     try{
-        // translates to SELECT * FROM accounts
-      res.json(await DataBase.select("*").from("accounts").where("id", req.params.id));
+        // const account = await DataBase("accounts").where("id", req.params.id).first()
+
+        // translates to SELECT * FROM accounts WHERE id = some kind of id value (remember if you add first() it will return an object instead of an id)
+        res.json(await DataBase.select("*").from("accounts").where("id", req.params.id));
     } catch(error){
-        next(error)
+        next(error);
     }
 })
 
+// need to create an account 
+router.post('/', async (req, res, next) => {
+    try{
+        // create payload to use it on the insert function
+        const payload = {
+           name: req.body.name,
+           budget: req.body.budget 
+        }
+
+        // translates to INSERT INTO accounts ( name, budget) values( some values)
+       const [id] =  await DataBase("accounts").insert(payload);
+       
+       // return the object created here
+       res.json(await DataBase("accounts").where("id", id).first());
+
+    } catch(error){
+        next(error);
+    }
+})
 
 
 module.exports = router;
