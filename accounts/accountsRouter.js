6a5a1cb76db1b9ab01c714b421ac7375cd2047e5
imxande <alerodriguezlarosa@gmail.com>
2020-01-23 02:22:27 -0500

    added router to handle requests

---
+++ accounts/accountsRouter.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+// will be using the router to handle request so I need data base in here 
+const DataBase = require('../data/dbConfig.js')
+
+// i need to create router using express to handle that
+const router = express.Router();
+
+// need to get all accounts using async code
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const express = require('express');
 
-const db = require('./data/dbConfig.js');
+// need to bring the router here
+const accountsRouter = require('./accounts/accountsRouter.js');
 
+// creates server
 const server = express();
 
+// teaches how to read json
 server.use(express.json());
 
+// end point here
+server.use('/api/accounts', accountsRouter);
+
+// checking if server is listening
 server.get('/', (req, res) =>{
-    res.send("Hello from the server")
+    res.send('I am ready and running!')
 })
+
 
 module.exports = server;
