d802821f1e0ab9b74ca35ae04eb0c2b12d41b8aa
grizzlybrodams <jddaley328@gmail.com$4josh@josh-pcgit config --global user.namegit config --global user.name grizzlybrodams>
2020-01-29 22:05:11 -0700

    finnshed mvp

--- queries.sql
+++ queries.sql
@@ -1,16 +1,27 @@
 -- Database Queries
 
 -- Find all customers with postal code 1010
+SELECT * FROM [Customers] where postalcode = 1010
 
 -- Find the phone number for the supplier with the id 11
 
+SELECT * FROM [Suppliers] where supplierId = 11
+
 -- List first 10 orders placed, sorted descending by the order date
+
+SELECT * FROM [Orders] where orderdate order by orderdate desc limit 10
 
 -- Find all customers that live in London, Madrid, or Brazil
 
+SELECT * FROM [Customers] where City in("London" , "Madrid") or Country in("Brazil");
+
 -- Add a customer record for "The Shire", the contact name is "Bilbo Baggins" the address is -"1 Hobbit-Hole" in "Bag End", postal code "111" and the country is "Middle Earth"
 
+INSERT into customers (CustomerName, ContactName, Address, City, PostalCode, Country) values ( "The Shire", "Bilbo Baggins", "1 Hobbit Hole", "Bag End","111", "Middle Earth");
+
 -- Update Bilbo Baggins record so that the postal code changes to "11122"
+
+Update Customers set PostalCode = '11122' where CustomerID = '92'
 
 -- (Stretch) Find a query to discover how many different cities are stored in the Customers table. Repeats should not be double counted
 
--- server.js
+++ server.js
@@ -6,4 +6,57 @@ const server = express();
 
 server.use(express.json());
 
+
+server.get("/", async (req,res)=> {
+    try {
+        const accounts = await db("accounts");
+        res.status(200).json({success: true, accounts});
+    } catch {
+        res.status(500).json({success: false, error:"Problem with request"});
+    }
+});
+
+server.post("/", async(req,res)=> {
+    try{
+        const accounts = await db("accounts").insert(req.body);
+        res.status(201).json({success:true, accounts});
+    } catch {
+        res.status(500).json({ success: false, error: "Problem with request." });
+    }
+});
+
+server.put("/:id", async(req,res)=>{
+   const {id} = req.params;
+    try{
+      const accounts = await db("accounts")
+      .where({id})
+      .update(req.body);
+      res.status(200).json({success:true, accounts});
+
+    } catch{
+        res.status(500).json({ success: false, error: "Problem with request." });
+    }
+});
+
+
+server.delete("/:id",  async(req,res)=>{
+    const { id } = req.params;
+  try {
+    await db("accounts")
+      .where({ id })
+      .del();
+    res
+      .status(200)
+      .json({
+        success: true,
+        message: `Account ${id} was successfully deleted`
+      });
+  } catch {
+    res.status(500).json({ success: false, error: "Problem with request." });
+  }
+});
+
+
+
+
 module.exports = server;
