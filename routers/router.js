6a83567a788359df500ff508f29749fcca1d1721
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2020-02-17 18:15:11 -0500

    [Add] added route for account by id db query, postman tested, working.

--- routers/router.js
+++ routers/router.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.get('/', (req, res) =>{
 
-    db.select('*').from('accounts')
+    db('accounts')
     .then(account => {
         res.status(200).json({account});
     })
@@ -14,6 +14,28 @@ router.get('/', (req, res) =>{
         res.status(500).json({error: "There was a problem w/database"});
     });
     
-})
+});
+
+router.get('/:id', (req, res) => {
+    db('accounts').where({id: req.params.id})
+    .then(account => {
+        res.status(200).json(account);
+    })
+    .catch(error => {
+        res.status(500).json({error: "There was a problem getting that account"});
+    });
+});
+
+router.post('/', (req, res) => {
+
+});
+
+router.put('/:id', (req, res) => {
+
+});
+
+router.delete('/:id', (req, res) => {
+
+});
 
 module.exports = router;
