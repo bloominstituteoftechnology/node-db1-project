c8a407118b9b9bf59e0500c19a65d086d9dbf573
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2020-02-17 18:37:24 -0500

    [Add] Added post to db method to api/accounts, postman tested, working

--- routers/router.js
+++ routers/router.js
@@ -17,17 +17,29 @@ router.get('/accounts', (req, res) =>{
 });
 
 router.get('/accounts/:id', (req, res) => {
-    db('accounts').where({id: req.params.id})
+
+    db('accounts')
+    .where({id: req.params.id})
+    .first() //returns the first element in the returned array from db
     .then(account => {
         res.status(200).json(account);
     })
     .catch(error => {
         res.status(500).json({error: "There was a problem getting that account"});
     });
+
 });
 
 router.post('/accounts', (req, res) => {
 
+    db('accounts')
+    .insert(req.body, 'id') //second param returns an array with and id
+    .then(ids => {
+        res.status(201).json(ids);
+    })
+    .catch(error => {
+        res.status(500).json({error: "There was a problem posting to database."});
+    })
 });
 
 router.put('/:id', (req, res) => {
