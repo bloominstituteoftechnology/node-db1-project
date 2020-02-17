d4ce6b09078494d04af25e0d51e429d0d8380c65
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2020-02-17 18:07:27 -0500

    [Add] Added db query for all accounts using database methods

--- routers/router.js
+++ routers/router.js
@@ -4,5 +4,16 @@ const db = require('../data/dbConfig.js');
 
 const router = express.Router();
 
+router.get('/', (req, res) =>{
+
+    db.select('*').from('accounts')
+    .then(account => {
+        res.status(200).json({account});
+    })
+    .catch(error => {
+        res.status(500).json({error: "There was a problem w/database"});
+    });
+    
+})
 
 module.exports = router;
