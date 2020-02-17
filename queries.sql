89dc1e6bb8ec788b2b95651b2e2911f6eca1ea3c
viscountfam <V3ctor42!>
2020-02-17 17:25:00 -0500

    first stretch completed

--- queries.sql
+++ queries.sql
@@ -27,5 +27,6 @@
     Where CustomerId == "92"
 
 -- (Stretch) Find a query to discover how many different cities are stored in the Customers table. Repeats should not be double counted
+    SELECT DISTINCT City FROM Customers;
 
 -- (Stretch) Find all suppliers who have names longer than 20 characters. You can use `length(SupplierName)` to get the length of the name
