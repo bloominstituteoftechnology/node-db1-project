59864b8b58acf36703a1d1eda510a81a34381e46
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2020-02-17 17:37:54 -0500

    [Add] Added SQL query formulas to queries.sql

--- queries.sql
+++ queries.sql
@@ -1,16 +1,23 @@
 -- Database Queries
 
 -- Find all customers with postal code 1010
+SELECT * FROM Customers WHERE postalcode='1010';
 
 -- Find the phone number for the supplier with the id 11
-
+SELECT * FROM Suppliers WHERE supplierid =11;
 -- List first 10 orders placed, sorted descending by the order date
-
+SELECT * FROM Orders WHER
 -- Find all customers that live in London, Madrid, or Brazil
+SELECT * FROM Customers WHERE city='London' OR city='Madrid' OR country='Brazil' ORDER BY country desc;
 
 -- Add a customer record for "The Shire", the contact name is "Bilbo Baggins" the address is -"1 Hobbit-Hole" in "Bag End", postal code "111" and the country is "Middle Earth"
+INSERT INTO Customers (CustomerName, ContactName, Address,City, PostalCode, Country) 
+VALUES ('The Shire','Bilbo Baggins', 'Bag End Bagshot Row', 'Hobbiton', '111', 'Middle Earth');
 
 -- Update Bilbo Baggins record so that the postal code changes to "11122"
+UPDATE Customers
+Set postalcode = '11122'
+WHERE customerid = 92;
 
 -- (Stretch) Find a query to discover how many different cities are stored in the Customers table. Repeats should not be double counted
 
