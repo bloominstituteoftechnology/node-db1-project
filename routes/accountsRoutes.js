b0b11381923fa47f1d48c752f7f1fed038fccbae
wesley.moody <wgm3190@gmail.com>
2020-02-17 16:01:19 -0600

    everything is awesome

--- routes/accountsRoutes.js
+++ routes/accountsRoutes.js
@@ -5,7 +5,7 @@ const Db = require("../data/dbConfig");
 //  GET =======>
 
 router.get("/", (req, res) => {
-  Db.get()
+  Db("accounts")
     .then(accounts => {
       res.status(200).json(accounts);
     })
@@ -33,21 +33,24 @@ router.get("/:id", (req, res) => {
 
 router.post("/", (req, res) => {
   const account = req.body;
-  if (account.project_id && account.description && account.notes) {
-    Db.insert(account)
-      .then(() => {
-        res.status(200).json({ message: "Add new account to database." });
+  if (account.name && account.budget) {
+    //  INSERT INTO Accounts (account)
+    Db("Accounts")
+      .insert(account)
+      .then(account => {
+        res
+          .status(200)
+          .json({ message: "Created new account.", response: account });
       })
-      .catch(() => {
+      .catch(err => {
         res.status(400).json({
-          message: "Error occurred when adding new account to database"
+          message: "Error occurred when adding new account to database",
+          error: err
         });
       });
   } else {
-    res.catch(() => {
-      res
-        .status(500)
-        .json({ message: "Unable to add new account to database." });
+    res.status(400).json({
+      message: "Unable to add new account to database due to missing data."
     });
   }
 });
@@ -56,35 +59,34 @@ router.post("/", (req, res) => {
 
 router.put("/:id", (req, res) => {
   const { id } = req.params;
-  const newAccount = req.body;
-  if ((newAccount.description, newAccount.notes, newAccount.project_id)) {
-    actionsDB.update(id, newAccount).then(() => {
-      res.status(200).json({ message: "Account successfully updated" });
+  Db("accounts")
+    .where({ id })
+    .update(req.body)
+    .then(updVal => {
+      res.json(updVal);
+    })
+    .catch(error => {
+      console.log("put error");
+      res.status(500).json({ message: "Update failed" });
     });
-  } else {
-    res.catch(() => {
-      res.status(400)({ message: "Error with request." });
-    });
-    res.catch(() => {
-      res.status(500).json({ message: "Error updating Account" });
-    });
-  }
 });
 
 //  DELETE =======>
 
 router.delete("/:id", (req, res) => {
   const { id } = req.params;
-  DB.remove(id);
-  then(account => {
-    res.json(account);
-  }).catch(() => {
-    res
-      .status(500)
-      .json({ message: "Error removing account from database." });
-  });
+  Db("accounts")
+    .where({ id })
+    .del()
+    .then(deleted => {
+      res.json(deleted);
+    })
+    .catch(error => {
+      console.log("delete error");
+      res
+        .status(500)
+        .json({ message: "Error removing account from database." });
+    });
 });
 
-
 module.exports = router;
-
