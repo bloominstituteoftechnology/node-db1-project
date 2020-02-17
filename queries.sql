a218ac876bb1755fd6ee208742defe60690abbc8
viscountfam <V3ctor42!>
2020-02-17 18:25:03 -0500

    second stretch complete

--- queries.sql
+++ queries.sql
@@ -30,3 +30,4 @@
     SELECT DISTINCT City FROM Customers;
 
 -- (Stretch) Find all suppliers who have names longer than 20 characters. You can use `length(SupplierName)` to get the length of the name
+    SELECT * From Suppliers Where length(SupplierName) > 20
