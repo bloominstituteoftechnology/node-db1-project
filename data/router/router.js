d1c887a0461d536e12948d83e03f4283d66b375e
Alexis <alexisdavalos.tech@gmail.com>
2020-02-18 17:08:05 -0500

    Stretch Queries | Limit Method to GET Route | CRUD Endpoints | MVP Queries

--- data/router/router.js
+++ data/router/router.js
@@ -3,156 +3,96 @@ const express = require('express');
 const db = require('../dbConfig.js')
 const router = express.Router();
 
-//Post Routes
-router.post('/', validateUser, (req, res) => {
-  db.insert(req.body)
-        .then(post => {
-            console.log(post);
-            res.status(201).json({message: "user created!", user: {...post}})
-        })
-        .catch(error => {
-            // log error to database
-            console.log(error);
-            res.status(500).json({
-                message: 'Internal Error Adding The User',
-            });
-        });
-});
-
-router.post('/:id/posts', validateUserId, validatePost, (req, res) => {
-  postDb.insert(req.body)
-        .then(posts => {
-            console.log(posts);
-            res.status(201).json({message: "Success", data: {...posts}})
-        })
-        .catch(error => {
-            // log error to database
-            console.log(error);
-            res.status(500).json({
-                message: 'Internal Error Adding The User Post',
-            });
-        });
-});
-
 //Get Routes
-
-router.get('/', (req, res) => {
-    db.get().then(users =>{
-      res.status(200).json({message: "Success", data: {...users}})
+router.get("/", (req, res) => {
+    console.log(req.query)
+    // list of posts
+    // select from posts
+    // all database operations return a promise
+    db.select("*")
+      .from("accounts").limit(req.query.limit)
+      .then(posts => {
+        res.status(200).json(posts);
+      })
+      .catch(error => {
+        console.log(error);
+  
+        res.status(500).json({ error: "failed to get the list of posts" });
+      });
+  });
+  
+router.get("/:id", (req, res) => {
+// a post by it's id
+// select * from posts where id = :id
+    getById(req.params.id)
+    .then(account => {
+    res.status(200).json(account);
     })
     .catch(error => {
-      // log error to database
-      console.log(error);
-      res.status(500).json({
-          message: 'Internal Error Adding The Users',
-      });
+    console.log(error);
+
+    res.status(500).json({ error: "failed to get the post" });
+    });
+});
+
+//Post Routes
+router.post("/", (req, res) => {
+// add a post
+// insert into posts () values ()
+db("accounts")
+    .insert(req.body, "id") // will generate a warning on console when using sqlite, ignore that
+    .then(ids => {
+    return getById(ids[0]).then(inserted => {
+        res.status(201).json(inserted);
+    });
     })
+    .catch(error => {
+    console.log(error);
+
+    res.status(500).json({ error: "failed to add the post" });
+    });
 });
 
-router.get('/:id', validateUserId, (req, res) => {
-  // do your magic!
-  db.getById(req.params.id)
-  .then(user => {
-    (user) ?
-    res.status(200).json({message: "Success", data: {...user}}) : 
-    res.status(404).json({message: "User Not Found"}) 
-  })
-  
-  .catch(error => {
-    // log error to database
+//Put Routes
+router.put("/:id", (req, res) => {
+// update a post
+const id = req.params.id;
+const changes = req.body;
+db("accounts")
+    .where({ id }) // remember to filter or all records will be updated (BAD PANDA!!)
+    .update(changes) 
+    .then(count => {
+    res.status(200).json(count);
+    })
+    .catch(error => {
     console.log(error);
-    res.status(500).json({
-        message: 'Internal Error Getting The User',
-    });
-  })
-});
 
-router.get('/:id/posts', (req, res) => {
-  postDb.get().then(posts =>{
-    console.log(posts);
-    res.status(200).json({message: "Success", data: {...posts}})
-  })
-  .catch(error => {
-    // log error to database
-    console.log(error);
-    res.status(500).json({
-        message: 'Internal Error Getting The Posts',
+    res.status(500).json({ error: "failed to update the post" });
     });
-  })
 });
 
 //Delete Routes
-router.delete('/:id', validateUserId, (req, res) => {
-  db.remove(req.params.id)
-  .then(user => {
-    (user > 0) ? 
-    res.status(200).json({message: "Successfully Deleted"}) :
-    res.status(404).json({message: "User Not Found"})
-  })
-  
-  .catch(error => {
-    // log error to database
+router.delete("/:id", (req, res) => {
+// removes a post
+const id = req.params.id;
+db("accounts")
+    .where({ id }) // remember to filter or all records will be deleted (BAD PANDA!!)
+    .del()
+    .then(count => {
+    res.status(200).json(count);
+    })
+    .catch(error => {
     console.log(error);
-    res.status(500).json({
-        message: 'Internal Deleting The User',
+
+    res.status(500).json({ error: "failed to remove the post" });
     });
-  })
-});
-//Put End Points
-router.put('/:id', validateUser, validateUserId, (req, res) => {
-  const changes = req.body;
-  (changes.name === '') ? 
-  res.status(400).json({message: 'required field name cannot be empty'}) :
-  db.update(req.params.id, changes)
-  .then(user => {
-    (user > 0) ?
-    res.status(200).json({message: "Successfully Updated", user: {...changes}}) :
-    res.status(404).json({message: "Could Not Find User To Update"})
-  })
-  .catch(error => {
-    // log error to database
-    console.log(error);
-    res.status(500).json({
-        message: 'Internal Editing The User',
-    });
-  })
 });
 
-//custom middleware
-//for checking user with only the id as param
-function validateUserId(req, res, next) {
-  db.getById(req.params.id).then(user => {
-    if (user) {
-        req.user = user;
-        console.log(`passed user ID check:`, req.user)
-        next();
-    } else {
-        res.status(404).json({ message: "The specified ID does not exist." });
-    }
-}).catch(err => console.log(err))
-}
-
-//for passing a body in
-function validateUser(req, res, next){
-  console.log('Validating User...');
-  if(Object.entries(req.body).length === 0 && req.body.constructor === Object){
-    return res.status(500).json({ message: "missing user data" });
-  } else if(!req.body.name){
-    return res.status(500).json({ message: "missing required name field" });
-  }else{
-    console.log('passed user check:', req.body);
-  }
-  next(); //valid passes check
-  }
-
-function validatePost(req, res, next){
-  console.log('Validating Post...');
-  if(Object.entries(req.body).length === 0 && req.body.constructor === Object){
-    return res.status(500).json({ message: "missing post data" });
-  } else if(!req.body.text || req.body.text === ''){
-    return res.status(500).json({ message: "missing required text field" });
-  }
-  next(); //valid passes check
-  }
 
 module.exports = router;
+
+function getById(id) {
+    return db("accounts")
+      .where({ id })
+      .first();
+  }
--- server.js
+++ server.js
@@ -2,8 +2,16 @@ const express = require('express');
 
 const db = require('./data/dbConfig.js');
 
+const router = require('./data/router/router.js')
+
 const server = express();
 
 server.use(express.json());
+
+server.use('/api/accounts', router);
+
+server.get('/', (req, res) =>{
+    res.send('<h2>Node DB-1 Project</h2>')
+})
 
 module.exports = server;
