765f0a313a1ea56c1b469af413ccf764f28a82fe
chiragthesia <harithesia@gmail.com>
2020-02-25 22:20:14 -0600

    SQL Implmented

--- server.js
+++ server.js
@@ -2,8 +2,15 @@ const express = require('express');
 
 const db = require('./data/dbConfig.js');
 
+const accountRouter = require('./routes/accountRoutes');
+
 const server = express();
 
 server.use(express.json());
+server.use('/accounts', accountRouter);
 
+server.use((err, req, res, next) => {
+	console.log(err);
+	res.status(err.statusCode || 500).json(err);
+});
 module.exports = server;
