726bddb7eba5cb9942fd8687f6181b25642acaa5
Damaged0ne <43980544+Damaged0ne@users.noreply.github.com>
2020-02-28 11:09:27 -0700

    Committing SQL Queries

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const server = require('./server.js');
+const server = require("./server.js");
 
 const PORT = process.env.PORT || 4000;
 
 server.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}...`);
+ console.log(`Listening on http://localhost:${PORT}`);
 });
