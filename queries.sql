8ade6ec7b31fbbc4a19f5fcab73daaf73fa04e85
Damaged0ne <43980544+Damaged0ne@users.noreply.github.com>
2020-02-28 11:15:48 -0700

    push SQL Queries after testing

--- queries.sql
+++ queries.sql
@@ -7,7 +7,7 @@ SELECT Phone FROM Suppliers WHERE SupplierID = 11;
 -- List first 10 orders placed, sorted descending by the order date
 SELECT * FROM Orders ORDER BY OrderDate DESC LIMIT 10;
 -- Find all customers that live in London, Madrid, or Brazil
-SELECT * FROM Customers WHERE city = 'Madrid' or city = 'Berlin' or city = 'Brazil';
+SELECT * FROM Customers WHERE city = 'London' or city = 'Madrid' or country = 'Brazil';
 -- Add a customer record for "The Shire", the contact name is "Bilbo Baggins" the address is -"1 Hobbit-Hole" in "Bag End", postal code "111" and the country is "Middle Earth"
 INSERT INTO Customers (CustomerName, ContactName, Address, City, PostalCode, Country) VALUES ('The Shire', 'Bilbo Baggins', '1 Hobbit Hole', 'Bag End', '111', 'Middle Earth')
 -- Update Bilbo Baggins record so that the postal code changes to "11122"
