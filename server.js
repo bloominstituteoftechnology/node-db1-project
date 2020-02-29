25293e849f9fc04f175dcb4a521e994faccf9606
CLYPR Technologies <cbanks@clypr.co>
2020-02-29 17:40:51 -0500

    Updated server.js

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
-const express = require('express');
+const express = require("express");
 
-const db = require('./data/dbConfig.js');
+const db = require("./data/dbConfig.js");
+const accountRouter = require("./accounts/router.js")
 
 const server = express();
 
 server.use(express.json());
+server.use('/api/accounts', router)
+
+servver.get('/', (req, res) => {
+    return res.send('<h2>This is in the server.</h2>')
+})
+
+server.use((err, req, res, next) => {
+    console.log(err)
+    return res.status(500).json({ message: 'Something in the server is wrong.'})
+})
 
 module.exports = server;
