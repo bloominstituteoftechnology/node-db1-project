938881f92f584929fa11df7c60d1047c8b15ecec
Amber Mohler <ambermohler@iMac.local>
2020-03-16 16:52:52 -0400

    completed first stretch goal

--- queries.sql
+++ queries.sql
@@ -31,7 +31,11 @@ SET PostalCode = '11122'
 WHERE CustomerID = 92;
 
 -- (Stretch) Find a query to discover how many different cities are stored in the Customers table. Repeats should not be double counted
-
+SELECT DISTINCT City
+FROM Customers
+ORDER BY City;
 
 -- (Stretch) Find all suppliers who have names longer than 20 characters. You can use `length(SupplierName)` to get the length of the name
-
+SELECT *
+FROM [Suppliers]
+WHERE length(SupplierName) >=20;
