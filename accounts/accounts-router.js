daed0a8727d0b3bbf1b14a811ea32bca7ac0876b
ricardo-ljr <ricardo.leitejr95@gmail.com>
2020-03-16 15:01:47 -0600

    Get BY ID works

--- accounts/accounts-router.js
+++ accounts/accounts-router.js
@@ -4,6 +4,8 @@ const db = require("../data/dbConfig");
 
 const router = express.Router();
 
+// GET ACCOUNTS
+
 router.get("/", (req, res) => {
   db.select("*")
     .from("accounts")
@@ -12,6 +14,26 @@ router.get("/", (req, res) => {
     })
     .catch(error => {
       res.status(500).json({ message: "Sorry there was an error", error });
+    });
+});
+
+// GET ACCOUNTS BY ID
+
+router.get("/:id", (req, res) => {
+  db("accounts")
+    .where({ id: req.params.id })
+    .first()
+    .then(row => {
+      if (row) {
+        res.status(200).json({ data: row });
+      } else {
+        res.status(404).json({ data: "Not Found" });
+      }
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json({ message: "There was an error with this id", error });
     });
 });
 
