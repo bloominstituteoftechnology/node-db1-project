8fdbc330b7aa4834f6148ad11859e805d8859ff1
blakelower <blakelower1999@gmail.com>
2020-04-13 13:22:52 -0700

    DELETE is working

--- accounts.js
+++ accounts.js
@@ -42,19 +42,35 @@ router.post("/", (req, res) => {
     });
 });
 
-router.put("/:id", (req,res) => {
-    const update = req.body;
-    const {id} = req.params;
-    db("accounts")
-    .where({id})
+router.put("/:id", (req, res) => {
+  const update = req.body;
+  const { id } = req.params;
+  db("accounts")
+    .where({ id })
     .update(update)
     .then((count) => {
-        if (count > 0){
-            res.status(200).json({message: "updated successfully"})
-        } else {
-            res.status(404).json({message: "Unsuccessful"})
-        }
-    })
-})
+      if (count > 0) {
+        res.status(200).json({ message: "updated successfully" });
+      } else {
+        res.status(404).json({ message: "Unsuccessful" });
+      }
+    });
+});
 
+router.delete("/:id", (req, res) => {
+    db("accounts")
+      .where("id", "=", req.params.id)
+      .del()
+      .then(count => {
+        if (count > 0) {
+          res.status(200).json(count);
+        } else {
+          res.status(404).json({ message: "not found" });
+        }
+      })
+      .catch(error => {
+        res.status(500).json({ message: "error deleting the account" });
+      });
+  });
+  
 module.exports = router;
