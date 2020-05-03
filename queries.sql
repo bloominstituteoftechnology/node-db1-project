359fe2864f99d79692a55dbdd8ac6b2e0a14e130
jacobvanwormer <sharpiejacob@gmail.com>
2020-05-02 18:55:56 -0700

    mvp

--- queries.sql
+++ queries.sql
@@ -13,7 +13,7 @@
 -- Add a customer record for "The Shire", the contact name is "Bilbo Baggins" the address is -"1 Hobbit-Hole" in "Bag End", postal code "111" and the country is "Middle Earth"
     INSERT INTO Customers (CustomerName, ContactName, "Address", City, PostalCode, Country ) VALUES ("The Shire", "Bilbo Baggins", "1 Hobbit Hole", "Bag End", "111", "Middle Earth")
 -- Update Bilbo Baggins record so that the postal code changes to "11122"
-    lCode = "11122" WHERE CustomerName = "The Shire";  
+    UPDATE Customers SET PostalCode = "11122" WHERE CustomerName = "The Shire";
 -- (Stretch) Find a query to discover how many different cities are stored in the Customers table. Repeats should not be double counted
 
 -- (Stretch) Find all suppliers who have names longer than 20 characters. You can use `length(SupplierName)` to get the length of the name
