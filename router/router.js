92f6b58de662ce166569592d5f86c4c28249959f
Matthew Kearney <matthewkearney@matthews-mbp.lan>
2020-05-11 17:03:48 -0500

    all crud done

--- router/router.js
+++ router/router.js
@@ -5,6 +5,7 @@ const express = require('express')
  const router = express.Router();
 
 
+ //get accounts
  router.get("/", (req,res)=>{
     db.select().from('accounts')
     .then(account=>{
@@ -16,7 +17,76 @@ const express = require('express')
 })
 
 
+//get accounts by id
+router.get('/:id', (req,res)=>{
+    db('accounts')
+    .where({id:req.params.id})
+    .first()
+    .then(post=>{
+        if(post){
+            res.status(200).json({data:post})
+        }else{
+            res.status(400).json({message:"Post not found"})
+        }
+    })
+    .catch(error=>{
+        res.status(500).json({message:"Error"})
+    })
+})
 
+
+//add new account
+router.post("/", (req,res )=>{
+    const postData=req.body;
+    db("accounts")
+    .insert(postData)
+    .then(post=>{
+        res.status(201).json(post)
+    })
+    .catch(err=>{
+        res.status(500).json({message: "cannot create"})
+    })
+})
+
+
+//update account
+router.put("/:id", (req,res)=>{
+    const {id}=req.params
+    const changes=req.body
+
+    db("accounts")
+    .where({id})
+    .update(changes)
+    .then(count=>{
+        if(count){
+            res.json({updated:count})
+        }else{
+            res.status(200).json(count)
+        }
+    })
+    .catch(err=>{
+        res.status(500).json({message: "cannot update "})
+    })
+
+})
+
+
+//delete account
+router.delete("/:id", (req,res)=>{
+    const {id}= req.params
+    db("accounts")
+    .where ({id})
+    .del({id})
+    .then(deleted=>{
+        res.status(200).json(deleted
+        )
+    })
+    .catch(
+        err=>{
+            res.status(500).json({message: "cannot delete"})
+        }
+    )
+})
 
 
  module.exports = router;
