2c49486141bf2e0c36ab9ca28f8a9d007cbd3f0f
Matthew Kearney <matthewkearney@matthews-mbp.lan>
2020-05-11 16:14:43 -0500

    get accounts

--- router/router.js
+++ router/router.js
@@ -5,6 +5,18 @@ const express = require('express')
  const router = express.Router();
 
 
+ router.get("/", (req,res)=>{
+    db.select().from('accounts')
+    .then(account=>{
+        res.json(account)
+    })
+    .catch(err=>{
+        res.status(500).json({message:"error", err})
+    })
+})
+
+
+
 
 
  module.exports = router;
