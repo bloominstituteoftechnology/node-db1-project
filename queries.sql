e88d102c38afe819ac784e77ce39408c1d34bb8f
Matthew Kearney <matthewkearney@matthews-mbp.lan>
2020-05-11 15:00:26 -0500

    queries done

--- queries.sql
+++ queries.sql
@@ -2,13 +2,32 @@
 
 -- Find all customers with postal code 1010
 
+SELECT * FROM Customers 
+ where postalcode = '1010';
+
 -- Find the phone number for the supplier with the id 11
+
+SELECT *
+ FROM orders
+ order by orderdate desc
+ limit 10;
 
 -- List first 10 orders placed, sorted descending by the order date
 
+SELECT * FROM Customers 
+ where City in ('London', 'Madrid') 
+ or Country = 'Brazil';
+
 -- Find all customers that live in London, Madrid, or Brazil
 
+INSERT INTO Customers
+ (customerName, contactName, [address], City, postalCode, Country) 
+ values('The Shire', 'Bilbo Baggins', '1 hobbit-hole', 'Bag End', '111', 'Middle Earth');
+
 -- Add a customer record for "The Shire", the contact name is "Bilbo Baggins" the address is -"1 Hobbit-Hole" in "Bag End", postal code "111" and the country is "Middle Earth"
+
+UPDATE customers set PostalCode = 11122 
+ WHERE ContactName = 'Bilbo Baggins'
 
 -- Update Bilbo Baggins record so that the postal code changes to "11122"
 
