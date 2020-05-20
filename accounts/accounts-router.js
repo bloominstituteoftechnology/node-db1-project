8065076b7ce2d84065ae5d504e680e4ea1dd1776
Ewa Czech <ewa.czech@hotmail.com>
2020-05-20 15:36:53 +0200

    add CRUD functionality

--- accounts/accounts-router.js
+++ accounts/accounts-router.js
@@ -2,14 +2,64 @@ const express = require('express');
 const db = require('../data/dbConfig');
 const router = express.Router();
 
-router.get('/', async (req, res, next) => {
-	try {
-		res.json({
-			message: 'Welcome',
+router.get('/', (req, res, next) => {
+	db('accounts')
+		.then((accounts) => {
+			res.status(200).json(accounts);
+		})
+		.catch((err) => {
+			res.status(500).json({ message: 'Failed to get accounts' });
 		});
-	} catch (err) {
-		next(err);
-	}
+});
+
+router.post('/', (req, res, next) => {
+	const newAccount = req.body;
+	db('accounts')
+		.insert(newAccount)
+		.then((newAccount) => {
+			res.status(200).json(newAccount);
+		})
+		.catch((err) => {
+			res.status(500).json({ message: 'Failed to add accounts' });
+		});
+});
+
+router.delete('/:id', (req, res, next) => {
+	const accountId = req.params.id;
+	db('accounts')
+		.where({ id: accountId })
+		.del()
+		.then((account) => {
+			if (account > 0) {
+				res.status(200).json({ id: Number(req.params.id) });
+			} else {
+				res.status(404).json({
+					message: 'The account with the specified ID does not exist.',
+				});
+			}
+		})
+		.catch((err) => {
+			res.status(500).json({ message: 'Failed to add accounts' });
+		});
+});
+
+router.put('/:id', (req, res, next) => {
+	const accountId = req.params.id;
+	db('accounts')
+		.where({ id: accountId })
+		.update(req.body)
+		.then((account) => {
+			if (account > 0) {
+				res.status(200).json({ id: Number(req.params.id) });
+			} else {
+				res.status(404).json({
+					message: 'The account with the specified ID does not exist.',
+				});
+			}
+		})
+		.catch((err) => {
+			res.status(500).json({ message: 'Failed to edit accounts' });
+		});
 });
 
 module.exports = router;
