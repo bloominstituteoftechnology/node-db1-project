e5e7f5091c206df316efcea433649602cbc2efa1
Abdulhamid Azizy <55733008+HamidAzizy@users.noreply.github.com>
2020-05-20 21:09:14 -0700

    Project done

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -3,6 +3,7 @@ const knex = require("knex");
 const knexfile = require("../knexfile.js");
 
 // change to "production" and update knexfile.js to use postgres.
-const database = "development";
+// const database = "development";
+const database = "production"
 
 module.exports = knex(knexfile[database]);
