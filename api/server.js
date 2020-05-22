00b6ef9a6e99e9681582fb1c182020ca8e0bd468
Lambda_School_Loaner_181 <lambda_school_loaner_181@Lambda-School-Loaner-181s-MacBook-Pro.local>
2020-05-22 15:08:16 -0500

    done

--- api/server.js
+++ api/server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 
 const db = require("../data/dbConfig.js");
+const Router = require('../router/router')
 
 const server = express();
 
 server.use(express.json());
+server.use('/api/accounts', Router)
 
 module.exports = server;
--- queries.sql
+++ queries.sql
@@ -2,16 +2,35 @@
 
 -- Find all customers with postal code 1010
 
+SELECT * FROM Customers 
+ where postalcode = '1010';
+
 -- Find the phone number for the supplier with the id 11
+
+SELECT *
+ FROM orders
+ order by orderdate desc
+ limit 10;
 
 -- List first 10 orders placed, sorted descending by the order date
 
+SELECT * FROM Customers 
+ where City in ('London', 'Madrid') 
+ or Country = 'Brazil';
+
 -- Find all customers that live in London, Madrid, or Brazil
 
+INSERT INTO Customers
+ (customerName, contactName, [address], City, postalCode, Country) 
+ values('The Shire', 'Bilbo Baggins', '1 hobbit-hole', 'Bag End', '111', 'Middle Earth');
+
 -- Add a customer record for "The Shire", the contact name is "Bilbo Baggins" the address is -"1 Hobbit-Hole" in "Bag End", postal code "111" and the country is "Middle Earth"
+
+UPDATE customers set PostalCode = 11122 
+ WHERE ContactName = 'Bilbo Baggins'
 
 -- Update Bilbo Baggins record so that the postal code changes to "11122"
 
 -- (Stretch) Find a query to discover how many different cities are stored in the Customers table. Repeats should not be double counted
 
--- (Stretch) Find all suppliers who have names longer than 20 characters. You can use `length(SupplierName)` to get the length of the name
+-- (Stretch) Find all suppliers who have names longer than 20 characters. You can use `length(SupplierName)` to get the length 
