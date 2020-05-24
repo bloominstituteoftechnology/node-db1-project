b0395aa34fc66b3a132f34be2a7fbaa1d095fe6b
Veto Ramirez <vetorobert3@gmail.com>
2020-05-23 22:02:51 -0700

    Cleaned up code, fixed routes, ran all CRUD operations in postman and all are completed.

--- router/router.js
+++ router/router.js
@@ -16,18 +16,18 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   try {
     const {id} = req.params;
-    const posts = await db('posts').where({id});
-    res.json(posts)
+    const accounts = await db('accounts').where({id});
+    res.json(accounts);
   } catch (err) {
     res.status(500).json({message: 'ID not found', error:err});
   }
 });
 
 router.post('/', async (req, res) => {
-  const postData = req.body;
+  const postAccounts = req.body;
   try {
-    const numPosts = await db('posts').insert(postData);
-    res.status(201).json(numPosts)
+    const numAccounts = await db('accounts').insert(postAccounts);
+    res.status(201).json(numAccounts)
   } catch (err) {
     res.status(500).json({message: 'problem with db', error:err});
   }
@@ -35,9 +35,9 @@ router.post('/', async (req, res) => {
 
 router.put('/:id', async (req, res) => {
   const {id} = req.params;
-  const newPost = req.body;
+  const updateAccounts = req.body;
   try {
-    const count = await db('posts').update(newPost).where({id})
+    const count = await db('accounts').update(updateAccounts).where({id})
     if (count) {
       res.json({updated: count});
     } else {
@@ -51,9 +51,9 @@ router.put('/:id', async (req, res) => {
 router.delete('/:id', async(req, res) => {
   const {id} = req.params;
   try {
-    const count = await db('posts').del().where({id});
-    if (count) {
-      res.json({deleted: count});
+    const compAccount = await db('accounts').del().where({id});
+    if (compAccount) {
+      res.json({deleted: compAccount});
     } else {
       res.status(404).json({messge: 'invalid id'});
     }
@@ -63,5 +63,3 @@ router.delete('/:id', async(req, res) => {
 });
 
 module.exports = router;
-
-module.exports = router
