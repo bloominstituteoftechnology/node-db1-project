b6a142b57adbb58113276f56cff292bab0e14ecf
Veto Ramirez <vetorobert3@gmail.com>
2020-05-23 21:40:30 -0700

    Fixing routes and directory changes to make project work.

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require("express");
 
-const Router = require('./router/router')
+const Router = require('./router/router.js')
 
 const server = express();
 
 server.use(express.json());
-server.use('/api/accounts', Router)
+
+server.use('/api/accounts', Router);
+
+server.get('/', (req, res) => {
+  res.status(200).json({api: "up"});
+});
 
 module.exports = server;
