303c7792f95e5b1e9a92420bc6f43efab9d54187
Antoinette <create@aplusmedia.info>
2020-05-24 23:25:31 -0400

    api are working

--- api/server.js
+++ api/server.js
@@ -6,4 +6,26 @@ const server = express();
 
 server.use(express.json());
 
+const port = 3000;
+
+server.get('/api/accounts', (req, res) => {
+    res.status(200).send("Hello Antoinette")
+});
+
+server.post('/api/accounts', (req, res) => {
+     title: req.body.title 
+    res.status(201).json({url: '/api/accounts', operation: 'POST'})
+})
+
+server.put('/api/accounts', (req, res) => {
+    res.status(200).json({url: '/api/accounts', operation: 'PUT'})
+})
+
+server.delete('/api/accounts', (req, res) => {
+    res.status(204);
+})
+server.listen(port, () => {
+    console.log(`Listening one port ${port}`)
+})
+
 module.exports = server;
