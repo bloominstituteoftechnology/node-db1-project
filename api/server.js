feb7dc7e7f957cca57a846e60baae1a093d6ae68
Antoinette <create@aplusmedia.info>
2020-05-26 08:56:39 -0400

    working on it

--- api/server.js
+++ api/server.js
@@ -6,31 +6,64 @@ const server = express();
 
 server.use(express.json());
 
-server.use('/', (req, res) => {
-    res.status(200).send('Hello Antoiette')
-})
-
-
 const port = 3000;
 
 server.get('/api/accounts', (req, res) => {
-    res.status(200).send("Hello Antoinette")
+    const db = req.body
+    console.log(req.body)
+    res.status(200).send(db)
 });
 
-server.post('/api/accounts', (req, res) => {
-     title: req.body.title 
-    res.status(201).json({url: '/api/accounts', operation: 'POST'})
+server.get('/api/accounts/name', (req, res) => {
+    res.status(200).json({url: '/api/accounts/name', operation: 'GET'});
 })
+
+server.get('/api/accounts/budget', (req, res) => {
+    res.status(200).json({url: '/api/accounts/name', operation: 'GET'});
+})
+
+server.post('/api/accounts', (req, res) => {
+   res.status(201).json({url: '/api/accounts', operation: 'POST'})
+})
+
+server.post('/api/accounts/name', (req, res) => {
+  res.status(201).json({url: '/api/accounts/name', operation: 'POST'})
+})
+
+server.post('/api/accounts/budget', (req, res) => {
+    res.status(201).json({url: '/api/accounts/budget', operation: 'POST'})
+  })
 
 server.put('/api/accounts', (req, res) => {
     res.status(200).json({url: '/api/accounts', operation: 'PUT'})
 })
 
+server.put('/api/accounts/name', (req, res) => {
+    res.status(200).json({url: '/api/accounts/name', operation: 'PUT'})
+})
+
+server.put('/api/accounts/budget', (req, res) => {
+    res.status(200).json({url: '/api/accounts/budget', operation: 'PUT'})
+})
+
 server.delete('/api/accounts', (req, res) => {
     res.status(204);
 })
+
+server.delete('/api/accounts/name', (req, res) => {
+    res.status(204);
+})
+
+server.delete('/api/accounts/budget', (req, res) => {
+    res.status(204);
+})
+
 server.listen(port, () => {
     console.log(`Listening one port ${port}`)
+})
+
+server.use('/', (req, res) => {
+    res.status(200).send('Hello Antoiette')
 })
 
 module.exports = server;
