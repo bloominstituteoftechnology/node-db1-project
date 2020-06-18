6ffb73a242a9c9a69ed40a6b2e3ad91aaa2f9749
thegroovetrain <ericthegroovetrain@gmail.com>
2020-06-17 20:42:25 -0400

    mvp

--- api/server.js
+++ api/server.js
@@ -6,4 +6,54 @@ const server = express();
 
 server.use(express.json());
 
+server.get('/api/accounts', (req, res) => {
+    db('accounts').then(accounts => {
+        res.status(200).json(accounts);
+    }).catch(err => {
+        res.status(500).json({message: 'Failed to get accounts'});
+    });
+});
+
+server.get('/api/accounts/:id', (req, res) => {
+    db('accounts').where({id: parseInt(req.params.id, 10)}).then(account => {
+        if (account.length) {
+            res.status(200).json(account);
+        } else {
+            res.status(404).json({message: 'ID not found'});
+        }
+    }).catch(err => {
+        res.status(500).json({message: 'Failed to get account'});
+    });
+});
+
+server.post('/api/accounts', (req, res) => {
+    db('accounts').insert({
+        name: req.body.name,
+        budget: req.body.budget
+    }).then(id => {
+        res.status(201).json(id);
+    }).catch(err => {
+        res.status(500).json({message: 'Failed to create account'});
+    });
+});
+
+server.put('/api/accounts/:id', (req, res) => {
+    db('accounts').where({id: parseInt(req.params.id, 10)}).update({
+        name: req.body.name,
+        budget: req.body.budget
+    }).then(count => {
+        res.status(200).json(count);
+    }).catch(err => {
+        res.status(500).json({message: 'Failed up update account'});
+    });
+});
+
+server.delete('/api/accounts/:id', (req, res) => {
+    db('accounts').where({id: parseInt(req.params.id, 10)}).del().then(count => {
+        res.status(200).json(count);
+    }).catch(err => {
+        res.status(500).json({message: 'Failed to delete account'});
+    });
+});
+
 module.exports = server;
