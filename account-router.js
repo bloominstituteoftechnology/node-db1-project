083590b1037e48c8972ab33aff5b00b63a31884a
c00kamunga <tsothalanti2@yahoo.com>
2020-06-19 19:36:36 -0700

    created GET request to get all accounts

---
+++ account-router.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const db = require("./data/dbConfig");
+
+const router = express.Router();
+
+router.get("/", async (req, res, next) => {
+  try {
+    const accounts = await db.select("*").from("accounts");
+
+    res.json(accounts);
+  } catch (err) {
+    next(err);
+  }
+});
+
+
+
+
+module.exports = router
