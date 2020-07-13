cc38051365907e69deef5194af468cf194111341
Lambda_School_Loaner_277 <lambda_school_loaner_277@Lambda-School-Loaner-277s-MacBook-Pro.local>
2020-07-13 16:10:23 -0400

    Complete and Working

--- queries.sql
+++ queries.sql
@@ -1,16 +1,32 @@
 -- Database Queries
 
 -- Find all customers with postal code 1010
+select * from customers
+ where postalCode = 1010
 
 -- Find the phone number for the supplier with the id 11
 
+SELECT phone FROM Suppliers 
+where supplierid = 11
+
 -- List first 10 orders placed, sorted descending by the order date
+
+select * from orders 
+order by orderdate desc 
+limit 10;
 
 -- Find all customers that live in London, Madrid, or Brazil
 
+select * from customers
+where city in ('London', 'Madrid', 'Brazil') 
+
 -- Add a customer record for "The Shire", the contact name is "Bilbo Baggins" the address is -"1 Hobbit-Hole" in "Bag End", postal code "111" and the country is "Middle Earth"
+insert into customers
+(CustomerName, ContactName, Address, City, PostalCode, Country) 
+values ('The Shire', 'Bilbo Baggins', '1 Hobbit-Hole', 'Bag End', '111', 'Middle Earth');
 
 -- Update Bilbo Baggins record so that the postal code changes to "11122"
+update customers set PostalCode = 11122 where customerId = 92;
 
 -- (Stretch) Find a query to discover how many different cities are stored in the Customers table. Repeats should not be double counted
 
