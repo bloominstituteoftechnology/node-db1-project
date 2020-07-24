5d6408f6eebaf814161d6fa48380d3669b4309b6
Brian Clayton <finalb@projectfinalboss.com>
2020-07-24 19:41:45 -0400

    update

--- knexfile.js
+++ knexfile.js
@@ -18,10 +18,10 @@ module.exports = {
   production: {
     client: "pg",
     connection: {
-      host: "localhost", // if the server is not running on your computer provide the network address
-      database: "the name of the database to use in the postgres server", // <-- update
-      user: "a user that has access to the server and database", // <-- update
-      password: "the password for the user", // <-- update
+      host: "http://localhost:5000", // if the server is not running on your computer provide the network address
+      database: "./data/budget.db3", // <-- update
+      user: "user", // <-- update
+      password: "admin", // <-- update
     },
     pool: {
       min: 2,
