77ae1ff2a2246acce399bffe87193feefae3f573
Brian Clayton <finalb@projectfinalboss.com>
2020-07-25 13:55:20 -0400

    update

--- api/server.js
+++ api/server.js
@@ -4,6 +4,19 @@ const db = require("../data/dbConfig.js");
 
 const server = express();
 
+
+
 server.use(express.json());
+
+server.get('/', (req, res) => {
+db.select('*').from('accounts')
+.then( acc => 
+res.status(200).json({data: acc})
+)
+.catch(
+    err => console.log(err)
+)
+})
+
 
 module.exports = server;
