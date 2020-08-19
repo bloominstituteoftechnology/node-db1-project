0dc568c48091117f92d392cd06c5c93263ae1b69
Vish Singh <vishalicious213@gmail.com>
2020-08-19 16:42:51 -0700

    getting single account from /accounts by id

--- api/server.js
+++ api/server.js
@@ -6,6 +6,7 @@ const server = express();
 
 server.use(express.json());
 
+// get all accounts
 server.get('/accounts', async (req, res, next) => {
     try { // SELECT * FROM "accounts"
         const accounts = await db.select("*").from("accounts")
@@ -13,6 +14,17 @@ server.get('/accounts', async (req, res, next) => {
     } catch (error) {
         next(error)
     }
-  });
+});
+
+// get single account
+server.get('/accounts/:id', async (req, res, next) => {
+    try { // SELECT '*' FROM "accounts" WHERE "id" = "req.params.id"
+        const account = await db.select("*").from("accounts").where("id", req.params.id).limit(1)
+        res.json(account)
+    } catch (error) {
+        next(error)
+    }
+});
+
 
 module.exports = server;
