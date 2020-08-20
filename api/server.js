10eb6c17bfe8bf96b220ae619e0dc7a33fde7575
Vish Singh <vishalicious213@gmail.com>
2020-08-19 17:19:34 -0700

    post new account into /accounts

--- api/server.js
+++ api/server.js
@@ -18,13 +18,31 @@ server.get('/accounts', async (req, res, next) => {
 
 // get single account
 server.get('/accounts/:id', async (req, res, next) => {
-    try { // SELECT '*' FROM "accounts" WHERE "id" = "req.params.id"
+    try { // SELECT '*' FROM "accounts" WHERE "id" = "req.params.id" LIMIT 1
         const account = await db.select("*").from("accounts").where("id", req.params.id).limit(1)
         res.json(account)
     } catch (error) {
         next(error)
     }
 });
+
+// post new account
+server.post('/accounts', async (req, res, next) => {
+    try { // INSERT INTO "accounts" ('name', 'budget') VALUES (req.body.name, req.body.budget)
+        const id = await db.insert({
+            name: req.body.name,
+            budget: req.body.budget
+        }).into("accounts")
+
+        // SELECT * FROM "accounts" WHERE "id" = "req.params.id" LIMIT 1
+        const account = await db("accounts").where("id", id).limit(1)
+        // NOTE: could also use .first() instead of .limit(1)
+        // const account = await db("accounts").where("id", id).first()
+        res.status(201).json(account)
+    } catch (error) {
+        next(error)
+    }
+})
 
 
 module.exports = server;
