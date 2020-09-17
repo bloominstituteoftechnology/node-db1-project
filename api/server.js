3c1e29c5aed8aaf1865e1d5b1cf9a19605961bd3
Ben Dennett <bendennett@gmail.com>
2020-09-16 20:48:59 -0600

    mvp ready

--- api/server.js
+++ api/server.js
@@ -1,9 +1,17 @@
 const express = require("express");
 
+const AccountRouter = require('../accounts/accountRouter.js');
+
 const db = require("../data/dbConfig.js");
 
 const server = express();
 
 server.use(express.json());
+
+server.use("/api/accounts", AccountRouter);
+
+server.get("/", (req, res) => {
+    res.status(200).json({ api: "up" });
+  });
 
 module.exports = server;
