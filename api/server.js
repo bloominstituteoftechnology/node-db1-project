6c26530cc9a8ca5ed91b0334520f0a54d7a1a6f3
Aeson Johnson <aeson.m.johnson@gmail.com>
2020-09-18 15:53:50 -0700

    initial commit + queries done

--- api/server.js
+++ api/server.js
@@ -1,9 +1,15 @@
 const express = require("express");
+const server = express();
 
 const db = require("../data/dbConfig.js");
 
-const server = express();
+
 
 server.use(express.json());
+const PORT = 5000;
+
+server.listen(PORT, () => {
+    console.log(`listening on port ${PORT}...`);
+});
 
 module.exports = server;
