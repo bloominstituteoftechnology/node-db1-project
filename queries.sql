2e534ce49f13c7f612de7ed94c791194c092e367
amichalo421 <andre.michalowski@gmail.com>
2020-11-01 15:12:39 -0500

    Queries reviewed

--- queries.sql
+++ queries.sql
@@ -1,31 +1,36 @@
--- Database Queries
 
--- Find all customers with postal code 1010
-SELECT * from customers;
-where postalCode = 1010;
+<!-- - find all customers with postal code 1010. Returns 3 records. -->
 
--- Find the phone number for the supplier with the id 11
-SELECT * from suppliers;
-where id = 11;
+SELECT * FROM Customers
+WHERE PostalCode='1010';
 
--- List first 10 orders placed, sorted descending by the order date
-SELECT * from orders;
-order by order date desc;
+<!-- - find the phone number for the supplier with the id 11. Should be (010) 9984510. -->
+
+SELECT * FROM Suppliers
+WHERE SupplierID="11";
+
+<!-- - list first 10 orders placed, sorted descending by the order date. The order with date 1997-02-12 should be at the top. -->
+
+SELECT * FROM Orders
+ORDER BY OrderDate desc
 limit 10;
 
--- Find all customers that live in London, Madrid, or Brazil
-SELECT * from customers;
-where city = London, city = Madrid, country = Brazil;
+<!-- - find all customers that live in London, Madrid, or Brazil. Returns 18 records. -->
 
--- Add a customer record for "The Shire", the contact name is "Bilbo Baggins" the address is -"1 Hobbit-Hole" in "Bag End", postal code "111" and the country is "Middle Earth"
-SELECT * from customers;
-insert into category ( name = Bilbo Baggins, Address = ..., ..., ..., ...);
+SELECT * FROM Customers
+WHERE city='London' or city='Madrid' or country='Brazil';
 
--- Update Bilbo Baggins record so that the postal code changes to "11122"
-SELECT * from customers;
-update category set postal code = 11122; --//added after filtered for bilbo
-where name = Bilbo Baggins;
+<!-- - add a customer record for _"The Shire"_, the contact name is _"Bilbo Baggins"_ the address is _"1 Hobbit-Hole"_ in _"Bag End"_, postal code _"111"_ and the country is _"Middle Earth"_. -->
 
+SELECT * FROM Customers
+INSERT INTO Customers (CustomerName, ContactName, Address, City, PostalCode, Country)
+VALUES ('The Shire', 'Bilbo Baggins', '1 Hobbit-Hole', 'Bag End', '111', 'Middle Earth');
+
+<!-- - update _Bilbo Baggins_ record so that the postal code changes to _"11122"_. -->
+
+SELECT * from Customers
+UPDATE customers set PostalCode = '11122'
+WHERE customerName='BilboBaggins';
 
 -- (Stretch) Find a query to discover how many different cities are stored in the Customers table. Repeats should not be double counted
 
