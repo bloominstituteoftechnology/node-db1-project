aee507e09e7dea9bd030a6efb90a1d11003730eb
Andrew Poppenberg <andrewpoppenberg@yahoo.com>
2020-12-07 14:24:45 -0600

    finished mvp

--- api/server.js
+++ api/server.js
@@ -3,7 +3,10 @@ const express = require("express");
 const db = require("../data/dbConfig.js");
 
 const server = express();
+const accountRouter = require('./accounts/account-router')
 
 server.use(express.json());
+server.use('/api/accounts', accountRouter)
+
 
 module.exports = server;
--- queries.sql
+++ queries.sql
@@ -2,15 +2,27 @@
 
 -- Find all customers with postal code 1010
 
+SELECT * FROM Customers where postalcode = '1010';
+
 -- Find the phone number for the supplier with the id 11
+
+SELECT phone from suppliers where supplierid = 11
 
 -- List first 10 orders placed, sorted descending by the order date
 
+SELECT * FROM [Orders]  order by Orderdate desc limit 10
+
 -- Find all customers that live in London, Madrid, or Brazil
+
+SELECT customername FROM [Customers] where city like'london'or city like'madrid' or city like'brazil'
 
 -- Add a customer record for "The Shire", the contact name is "Bilbo Baggins" the address is -"1 Hobbit-Hole" in "Bag End", postal code "111" and the country is "Middle Earth"
 
+INSERT into Customers (CustomerName, Address, City, Country, Postalcode, country) values ('Bilbo Baggins', '1 Hobbit Way', 'The Shire', '111', 'Middle Earth')
+
 -- Update Bilbo Baggins record so that the postal code changes to "11122"
+
+update cusomers set postalcode ='11122' where customerid = 92
 
 -- (Stretch) Find a query to discover how many different cities are stored in the Customers table. Repeats should not be double counted
 
