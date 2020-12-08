647fc4eca4d6d06da9f989c834461bf5a784ffc1
Cristian Fernandez <cristian.fernandez123@yahoo.com>
2020-12-07 16:36:44 -0800

    finished crud

--- api/server.js
+++ api/server.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const CustomerRouter = require('./customers/customer-router'); 
 
 const db = require("../data/dbConfig.js");
 
 const server = express();
-
 server.use(express.json());
+server.use("/api", CustomerRouter); 
 
 module.exports = server;
