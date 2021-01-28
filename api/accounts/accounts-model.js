9eb0f4318e58d657ffbfa12f7475f90807c19df4
Austin Littlefield <austinlittlefield@MacBook-Pro.local>
2021-01-27 20:28:14 -0800

    Wrote endpoints

---
+++ api/accounts/accounts-model.js
@@ -0,0 +1,9 @@
+const db = require('../../data/dbConfig.js')
+
+module.exports = {
+    get,
+    getByID,
+    create,
+    update,
+    remove,
+}
--- data/dbConfig.js
+++ data/dbConfig.js
@@ -3,6 +3,6 @@ const knex = require("knex");
 const knexfile = require("../knexfile.js");
 
 // change to "production" and update knexfile.js to use postgres.
-const database = "development";
+const database = "production";
 
 module.exports = knex(knexfile[database]);
