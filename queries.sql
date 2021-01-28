e9a92bdf837d61e1194f8543a24df3cd795dd891
extrude575757 <nicholas.a.lacapria@gmail.com>
2021-01-27 21:50:38 -0800

    Updated with sql

--- queries.sql
+++ queries.sql
@@ -11,6 +11,10 @@ LIKE	Search for a pattern
 IN	To specify multiple possible values for a column	
 
 
+ALTER TABLE Customers 
+ADD "The Shire" varchar(255);
+
+
 -- Database Queries
 
 -- Find all customers with postal code 1010
@@ -24,12 +28,25 @@ SELECT * FROM [Suppliers]
 WHERE SupplierID=11
 -- List first 10 orders placed, sorted descending by the order date
 
+SELECT * FROM [Orders]  
+Order by OrderDate ASC, OrderDate ASC LIMIT 10;
 -- Find all customers that live in London, Madrid, or Brazil
+SELECT * FROM [Customers]  
+WHERE Country='Brazil' OR Country='Madrid' OR Country='London'
+-- Add a customer record for "The Shire", the contact name is "Bilbo Baggins" 
+-- the address is -"1 Hobbit-Hole" in "Bag End", postal code "111" and the country is
+--  "Middle Earth"
 
--- Add a customer record for "The Shire", the contact name is "Bilbo Baggins" the address is -"1 Hobbit-Hole" in "Bag End", postal code "111" and the country is "Middle Earth"
+
+INSERT INTO Customers (CustomerName, ContactName, Address, City, PostalCode, Country)
+VALUES ('The Shire', 'Bilbo Baggins', '1 Hobbit-Hole', 'Bag End', '111', 'Middle Earth');
 
 -- Update Bilbo Baggins record so that the postal code changes to "11122"
+UPDATE Customers
+SET PostalCode='11122'
+WHERE ContactName='Bilbo Baggins';
+-- (Stretch) Find a query to discover how many different cities are stored in the 
+--Customers table. Repeats should not be double counted
 
--- (Stretch) Find a query to discover how many different cities are stored in the Customers table. Repeats should not be double counted
-
--- (Stretch) Find all suppliers who have names longer than 20 characters. You can use `length(SupplierName)` to get the length of the name
+-- (Stretch) Find all suppliers who have names longer than 20 characters. 
+-- You can use `length(SupplierName)` to get the length of the name
