3a5f362db2acbea9aa1da57fc01da2b02cf777da
Diego Roman <diegormnv209@gmail.com>
2021-02-15 13:24:46 -0800

    get by id works

--- api/accounts/account-router.js
+++ api/accounts/account-router.js
@@ -13,4 +13,14 @@ router.get('/', async(req, res, next) =>{
     }
 })
 
+router.get('/:id', validateId, async (req, res, next) =>{
+    try{
+        const { id } = req.params
+        const data = await Account.getById(id)
+        res.status(200).json(data)
+    } catch(error){
+        next(error)
+    }
+})
+
 module.exports = router;
