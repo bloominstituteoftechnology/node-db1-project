c7f09d0c6c494571ef2a495bef4253a09fd6a1c5
ndossett <niki.m.dossett@gmail.com>
2021-02-17 10:00:41 -0500

    set up accounts router, accounts model and server

---
+++ api/accountsRouter.js
@@ -0,0 +1,36 @@
+const express = require("express");
+
+const accountsDb = require("./accountsModel");
+
+const router = express.Router();
+
+const validateID = (req, res, next) => {
+
+};
+
+const validatePost = (req, res, next) => {
+
+};
+
+router.get("/", (req, res, next) => {
+
+});
+
+router.get("/:id", (req, res, next) => {
+    
+});
+
+router.post("/", (req, res, next) => {
+    
+});
+
+router.put("/:id", (req, res, next) => {
+    
+});
+
+router.delete("/", (req, res, next) => {
+    
+});
+
+
+module.exports = router
--- api/server.js
+++ api/server.js
@@ -1,9 +1,15 @@
 const express = require("express");
 
-const db = require("../data/dbConfig.js");
+const accountsRouter = require("./accountsRouter");
 
 const server = express();
 
 server.use(express.json());
+
+server.use("/api/accounts", accountsRouter);
+
+server.get("/", (_,res) => {
+    res.json("Welcome to the accounts api")
+});
 
 module.exports = server;
