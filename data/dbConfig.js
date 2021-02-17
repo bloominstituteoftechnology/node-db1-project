7a46ff48effa39566ef4eb0c8e416430f1f7184b
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-02-17 20:46:05 +0100

    update for codegrade

--- data/dbConfig.js
+++
@@ -1,8 +0,0 @@
-const knex = require("knex");
-
-const knexfile = require("../knexfile.js");
-
-// change to "production" and update knexfile.js to use postgres.
-const database = "development";
-
-module.exports = knex(knexfile[database]);
--- knexfile.js
+++ knexfile.js
@@ -1,38 +1,26 @@
+const sharedConfig = {
+  client: 'sqlite3',
+  useNullAsDefault: true,
+  migrations: {
+    directory: './data/migrations',
+  },
+  seeds: {
+    directory: './data/seeds',
+  },
+  pool: {
+    afterCreate: (conn, done) => {
+      conn.run('PRAGMA foreign_keys = ON', done)
+    },
+  },
+}
+
 module.exports = {
   development: {
-    client: "sqlite3",
-    connection: {
-      filename: "./data/budget.db3",
-    },
-    useNullAsDefault: true,
-    migrations: {
-      directory: "./data/migrations",
-      tableName: "knex_migrations",
-    },
-    seeds: {
-      directory: "./data/seeds",
-    },
+    ...sharedConfig,
+    connection: { filename: './data/budget.db3' },
   },
-
-  // update the following configuration to use PostgreSQL
-  production: {
-    client: "pg",
-    connection: {
-      host: "localhost", // if the server is not running on your computer provide the network address
-      database: "the name of the database to use in the postgres server", // <-- update
-      user: "a user that has access to the server and database", // <-- update
-      password: "the password for the user", // <-- update
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      directory: "./data/migrations",
-      tableName: "knex_migrations",
-    },
-    seeds: {
-      directory: "./data/seeds",
-    },
+  testing: {
+    ...sharedConfig,
+    connection: { filename: './data/testing.db3' },
   },
-};
+}
