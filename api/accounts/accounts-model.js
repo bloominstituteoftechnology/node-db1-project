b0ff69227ec98fadbc9fae758068ee03be4426da
IsaacGHoward <howardisaac100@gmail.com>
2021-02-24 17:01:20 -0800

    Created the accounts-model.js

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,21 +1,37 @@
+const db = require('../../data/db-config');
+
 const getAll = () => {
   // DO YOUR MAGIC
+  return db('accounts');
 }
 
 const getById = id => {
   // DO YOUR MAGIC
+  return db('accounts')
+    .where({id})
 }
 
 const create = async account => {
   // DO YOUR MAGIC
+  return db('accounts')
+    .insert(account)
+    .then(ids => {
+      return getById(ids[0])
+    });
 }
 
 const updateById = async (id, account) => {
   // DO YOUR MAGIC
+  return db('accounts')
+    .where({ id })
+    .update(account);
 }
 
 const deleteById = async id => {
   // DO YOUR MAGIC
+  return db('accounts')
+    .where('id', id)
+    .del();
 }
 
 module.exports = {
