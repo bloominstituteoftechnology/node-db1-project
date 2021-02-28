7dad4c8ed6af6f77e8623f9044ef6b8de32608b0
Your Name <Reginaldbmiller@gmail.com>
2021-02-28 13:43:05 -0600

    updates

--- api/server.js
+++ api/server.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const accountsRouter = require("./accounts/accounts-router")
 
 const server = express();
-
+server.use(express.urlencoded({extended:false}))
 server.use(express.json());
+server.use("/", accountsRouter)
 
 module.exports = server;
