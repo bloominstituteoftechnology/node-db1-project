8f1d7e4a62cf6ccb65c17ef0c692d3c664c3c620
Ahmad Wali Sayadi <sayadi@Ahmads-MacBook-Pro.local>
2021-03-01 15:32:32 -0800

    project completed

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,31 +1,80 @@
-const router = require('express').Router()
+const router = require("express").Router();
+const db = require("../../data/db-config");
 
-router.get('/', async (req, res, next) => {
-  // DO YOUR MAGIC
-})
+router.get("/", async (req, res, next) => {
+  try {
+    const accounts = await db.select("*").from("accounts");
 
-router.get('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
-})
-
-router.post('/', (req, res, next) => {
-  // DO YOUR MAGIC
-})
-
-router.put('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+    res.json(accounts);
+  } catch (err) {
+    next(err);
+  }
 });
 
-router.delete('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
-})
+router.get("/:id", async (req, res, next) => {
+  try {
+    const accoute = await db
+      .select("*")
+      .from("accouts".where("id", req.params.id));
+  } catch (err) {
+    next(err);
+  }
+});
 
-router.use((err, req, res, next) => { // eslint-disable-line
+router.post("/", async (req, res, next) => {
+  try {
+    const payload = {
+      name: req.body.name,
+      budget: req.body.budget,
+    };
+
+    const [accountID] = await db.insert(payload).into("accounts");
+
+    const account = await db.first("*").from("accounts").where("id", accountID);
+
+    res.status(201).json(account);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.put("/:id", async (req, res, next) => {
+  try {
+    const payload = {
+      name: req.body.name,
+      budget: req.body.budget,
+    };
+
+    await db("accounts").update(payload).where("id", req.params.id);
+
+    const account = await db
+      .first("*")
+      .from("accounts")
+      .where("id", req.params.id);
+
+    res.json(account);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.delete("/:id", async (req, res, next) => {
+  try {
+    await db("accounts").where("id", req.params.id).del();
+
+    res.status(204).end();
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.use((err, req, res, next) => {
+  // eslint-disable-line
   // CALL next(err) IF THE PROMISE REJECTS INSIDE YOUR ENDPOINTS
   res.status(500).json({
-    message: 'something went wrong inside the accounts router',
+    message: "something went wrong inside the accounts router",
     errMessage: err.message,
-  })
-})
+  });
+});
 
 module.exports = router;
