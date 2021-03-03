9e500a918050175947ddd23ce9d14a0ba2667aad
Amber Chunn <a.77@me.com>
2021-03-02 19:46:08 -0500

    Completed MVP

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const server = require("./api/server.js");
+const server = require('./api/server.js');
 
-const PORT = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
-server.listen(PORT, () => {
-  console.log(`\n== API running on port ${PORT} ==\n`);
-});
+server.listen(port, () => {
+  console.log(`\n== API running on port ${port} ==\n`);
+})
