49c9579668e271e180cf3da007853f1462242b2e
Vince Garcia <vinceryangarcia@gmail.com>
2021-03-16 15:15:22 -0700

    MVP

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,27 +1,54 @@
 const router = require('express').Router()
+const Account = require('./accounts-model')
+
+const { checkAccountPayload, checkAccountId, checkAccountNameUnique } = require('./accounts-middleware');
 
 router.get('/', async (req, res, next) => {
-  // DO YOUR MAGIC
+  try {
+    const data = await Account.getAll()
+    res.json(data)
+  } catch (err) {
+    next(err)
+  }
 })
 
-router.get('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+router.get('/:id', checkAccountId, async (req, res, next) => {
+  try {
+    const data = await Account.getById(req.params.id)
+    res.json(data)
+  } catch (err) {
+    next(err)
+  }
 })
 
-router.post('/', (req, res, next) => {
-  // DO YOUR MAGIC
+router.post('/', checkAccountPayload, checkAccountNameUnique, async (req, res, next) => {
+  try {
+    const data = await Account.create()
+    res.json(data)
+  } catch (err) {
+    next(err)
+  }
 })
 
-router.put('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+router.put('/:id', checkAccountPayload, checkAccountId, checkAccountNameUnique, async (req, res, next) => {
+  try {
+    const data = await Account.update(req.params.id, req.body)
+    res.json(data)
+  } catch (err) {
+    next(err)
+  }
 });
 
-router.delete('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+router.delete('/:id', checkAccountId, async (req, res, next) => {
+  try {
+    const data = await Account.remove(req.params.id)
+    res.json(data)
+  } catch (err) {
+    next(err)
+  }
 })
 
-router.use((err, req, res, next) => { // eslint-disable-line
-  // CALL next(err) IF THE PROMISE REJECTS INSIDE YOUR ENDPOINTS
+router.use((err, req, res, next) => { 
   res.status(500).json({
     message: 'something went wrong inside the accounts router',
     errMessage: err.message,
--- api/server.js
+++ api/server.js
@@ -1,7 +1,12 @@
 const express = require("express");
-
+const AccountsRouter = require('./accounts/accounts-router')
 const server = express();
 
 server.use(express.json());
+server.use("/api/accounts", AccountsRouter)
+
+server.get('/', (req, res) => {
+    res.json({ api: 'up' })
+})
 
 module.exports = server;
