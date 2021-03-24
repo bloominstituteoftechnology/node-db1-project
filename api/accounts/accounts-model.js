bb559dc5bd85badc497c712289b1a77bb20cd704
kato hawkins <kristhahawkins@icloud.com>
2021-03-24 02:55:57 -0400

    MVP

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,21 +1,33 @@
+const db =require('../../data/db-config.js')
+
 const getAll = () => {
-  // DO YOUR MAGIC
+  return db('accounts')
 }
 
 const getById = id => {
-  // DO YOUR MAGIC
+  return db("accounts").where("id",id).first()
 }
 
 const create = async account => {
-  // DO YOUR MAGIC
+  return db('accounts').insert(account)
+  .then(([id]) =>{
+    return db('accounts').where('id',id).first()
+  })
 }
 
 const updateById = async (id, account) => {
-  // DO YOUR MAGIC
+  const accountId = id
+  return db('accounts').where('id',id).update(account)
+  .then(() =>{
+    return db('accounts').where('id',accountId).first()
+  })
 }
 
 const deleteById = async id => {
-  // DO YOUR MAGIC
+  return db('accounts').where('id',id).del(id)
+  .then(() =>{
+    return db('accounts')
+  })
 }
 
 module.exports = {
--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,23 +1,57 @@
 const router = require('express').Router()
+const Account = require('./accounts-model')
+const mw = require('../accounts/accounts-middleware.js')
+
+
 
 router.get('/', async (req, res, next) => {
-  // DO YOUR MAGIC
+  try{
+    const data = await Account.getAll()
+    res.json(data)
+  }catch(err){
+    next(err)
+  }
 })
 
-router.get('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+router.get('/:id', mw.checkAccountId, async (req, res, next) => {
+  try{
+    const {id} = req.params
+    const data = await Account.getById(id)
+    res.json(data)
+  }catch(err){
+    next(err)
+  }
 })
 
-router.post('/', (req, res, next) => {
-  // DO YOUR MAGIC
+router.post('/', mw.checkAccountPayload, async(req, res, next) => {
+  try{
+    const account = req.body
+    const data = await Account.create(account)
+    res.json(data)
+  }catch(err){
+    next(err)
+  }
 })
 
-router.put('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+router.put('/:id',mw.checkAccountPayload, mw.checkAccountId,async (req, res, next) => {
+  try{
+    const {id} = req.params
+    const changes = req.body
+    const data = await Account.updateById(id,changes)
+    res.json(data)
+  }catch(err){
+    next(err)
+  }
 });
 
-router.delete('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+router.delete('/:id',mw.checkAccountId, async (req, res, next) => {
+  try{
+    const {id} = req.params
+    const data = await Account.deleteById(id)
+    res.json(data)
+  }catch(err){
+    next(err)
+  }
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
