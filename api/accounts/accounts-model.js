74edd0348b5e9692156ffd7c9902d5547b34146b
ngwatso <ngwatson79@yahoo.com>
2021-03-24 20:39:14 -0400

    completed accounts-model

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,27 +1,36 @@
+const db = require('../../data/db-config');
+
 const getAll = () => {
-  // DO YOUR MAGIC
-}
+	// !! DO YOUR MAGIC
+	return db('accounts');
+};
 
-const getById = id => {
-  // DO YOUR MAGIC
-}
+const getById = (id) => {
+	// !! DO YOUR MAGIC
+	const account = db.first('*').from('accounts').where({ id });
+	return account;
+};
 
-const create = async account => {
-  // DO YOUR MAGIC
-}
+const create = async (account) => {
+	// !! DO YOUR MAGIC
+	const newAccount = await db('accounts').insert(account);
+	return newAccount;
+};
 
 const updateById = async (id, account) => {
-  // DO YOUR MAGIC
-}
+	// !! DO YOUR MAGIC
+	return await db('accounts').update(account).where({ id });
+};
 
-const deleteById = async id => {
-  // DO YOUR MAGIC
-}
+const deleteById = async (id) => {
+	// !! DO YOUR MAGIC
+	return await db('accounts').del().where({ id });
+};
 
 module.exports = {
-  getAll,
-  getById,
-  create,
-  updateById,
-  deleteById,
-}
+	getAll,
+	getById,
+	create,
+	updateById,
+	deleteById,
+};
