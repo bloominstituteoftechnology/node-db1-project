c8bd817991b7b960ccdaf4391f8fabbe6823d5ad
jon del toro <61124054+jdeltoro88@users.noreply.github.com>
2021-03-24 22:39:03 -0600

    update

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,3 +1,19 @@
+
+
+
+/*
+- `getAll` resolves to an array of accounts (or an empty array)
+- `getById` resolves to an account by the given id
+- `create` resolves to the newly created account
+- `updateById` resolves to the updated account
+- `deleteById` resolves to the deleted account
+*/
+
+
+const db = require('../../data/db-config');
+
+
+
 const getAll = () => {
   // DO YOUR MAGIC
 }
