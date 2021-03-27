548d0437ad13acf7c6dbc0153c436b28db089eb7
jon del toro <61124054+jdeltoro88@users.noreply.github.com>
2021-03-26 21:53:35 -0600

    model

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,28 +1,33 @@
+const db =require('../../data/db-config.js')
+
 const getAll = () => {
-  return db ('accounts')
-  // DO YOUR MAGIC
+  return db('accounts')
 }
 
 const getById = id => {
-  if 9id
- {
-   return db ('accounts')
-   .where ('id', id)
-   .first()
- }  // DO YOUR MAGIC
+  return db("accounts").where("id",id).first()
 }
 
 const create = async account => {
-  
-  // DO YOUR MAGIC
+  return db('accounts').insert(account)
+  .then(([id]) =>{
+    return db('accounts').where('id',id).first()
+  })
 }
 
 const updateById = async (id, account) => {
-  // DO YOUR MAGIC
+  const accountId = id
+  return db('accounts').where('id',id).update(account)
+  .then(() =>{
+    return db('accounts').where('id',accountId).first()
+  })
 }
 
 const deleteById = async id => {
-  // DO YOUR MAGIC
+  return db('accounts').where('id',id).del(id)
+  .then(() =>{
+    return db('accounts')
+  })
 }
 
 module.exports = {
