4c7c6cff252c06ac0da856288ec5b80b3c7325b2
dustin-brinkman <mimirs.spring@gmail.com>
2021-04-15 15:01:51 -0400

    mvp

--- api/server.js
+++ api/server.js
@@ -1,7 +1,15 @@
 const express = require("express");
-
+const accountsRouter = require('../api/accounts/accounts-router');
 const server = express();
 
 server.use(express.json());
+server.use('/api/accounts', accountsRouter);
 
+server.use((err, req, res, next) => {
+    res.status(500).json({
+      message: err.message,
+      stack: err.stack,
+      custom: 'not sure what went wrong, but you done goofed.'
+    })
+  })
 module.exports = server;
--- queries.sql
+++ queries.sql
@@ -1,17 +1,36 @@
 -- Database Queries
 
 -- Find all customers with postal code 1010
+select * 
+from customers 
+where postalcode = 1010;
 
 -- Find the phone number for the supplier with the id 11
+select phone 
+from suppliers
+where supplierid = 11;
 
 -- List first 10 orders placed, sorted descending by the order date
+select * 
+from orders 
+order by orderdate desc 
+limit 10
 
 -- Find all customers that live in London, Madrid, or Brazil
+select city, country 
+from customers 
+where country like 'brazil' 
+or city like 'london' 
+or city like 'madrid';
 
--- Add a customer record for "The Shire", the contact name is "Bilbo Baggins" the address is -"1 Hobbit-Hole" in "Bag End", postal code "111" and the country is "Middle Earth"
-
+-- Add a customer record for "The Shire", the contact name is "Bilbo Baggins" the address is "1- Hobbit-Hole" in "Bag End", postal code "111" and the country is "Middle Earth"
+insert into customers (customername, contactname, address, city, postalcode, country)
+values ('The Shire', 'Bilbo Baggins', '1-Hobbit-Hole', 'Bag End', 111, 'Middle Earth')
 -- Update Bilbo Baggins record so that the postal code changes to "11122"
-
+update customers
+set postalcode = 11122
+where customerid = 92
 -- (Stretch) Find a query to discover how many different cities are stored in the Customers table. Repeats should not be double counted
 
 -- (Stretch) Find all suppliers who have names longer than 20 characters. You can use `length(SupplierName)` to get the length of the name
+
