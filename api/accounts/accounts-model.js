f7f2b4ed1f04aac1ee41d3c44657d089b512537d
daniel-vazquez <da.vazquez728@gmail.com>
2021-04-21 17:35:37 -0700

    accounts-model complete, ready to build router/server

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,21 +1,29 @@
+const db = require("../../data/db-config")
+
+
 const getAll = () => {
   // DO YOUR MAGIC
+  db.select("*")
 }
 
 const getById = id => {
   // DO YOUR MAGIC
+  db.select("*").from("accounts").where(id)
 }
 
-const create = account => {
+const create = async account => {
   // DO YOUR MAGIC
+  await db("accounts").insert(account)
 }
 
-const updateById = (id, account) => {
+const updateById = async (id, account) => {
   // DO YOUR MAGIC
+  await db("accounts").update(account).where(id)
 }
 
-const deleteById = id => {
+const deleteById = async id => {
   // DO YOUR MAGIC
+  await db("accounts").where(id).del()
 }
 
 module.exports = {
