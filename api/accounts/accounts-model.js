42c904e30f4fe4e4b0e6830a0d18b7ff99c4d7e2
Rhiannon Stanford <mochi@Rhiannons-iMac.local>
2021-06-16 14:22:27 -0700

    Complete router and model

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -3,33 +3,34 @@ const db = require('../../data/db-config');
 
 
 const getAll = () => {
-  // DO YOUR MAGIC
   return db("accounts") // select * from accounts
   // return db("accounts").select("contents") // select * from accounts, just return contents column, return as array of obj
 }
 
 const getById = id => {
-  // DO YOUR MAGIC
   // select * from accounts where accountid = userGivenId
   //  "id" refers to column id, id returns to the passed id
   return db("accounts").where("id", id).first()
-  // .first() gives you just the object
+  // .first() gives you just the object ... not in an array [{}]
 }
 
-const create = async (account) => {
-  // DO YOUR MAGIC
-  const [id] = await db("accounts").insert({account})
+const create = async (newAccount) => {
+  const {name, budget} = newAccount
+  const [id] = await db("accounts").insert({name, budget})
   return getById(id)
 }
 
-// async function create({account}) {
-//   const [id] = await db("accounts").insert({title,contents})
-//   return getById(id)
-// }
+/*
+An alternate way to write it using named function:
+ async function create({account}) {
+  const [id] = await db("accounts").insert({title,contents})
+  return getById(id)
+}
+*/
 
-const updateById = async (id, account) => {
-  // DO YOUR MAGIC
-  await db("accounts").where("id", id).update({account})
+const updateById = async (id, {name, budget}) => {
+  id = Number(id); // convert passed string to int
+  await db("accounts").where("id", id).update({name, budget})
   return getById(id)
 }
 
@@ -47,3 +48,7 @@ module.exports = {
   updateById,
   deleteById,
 }
+
+// curl -d '{"name":"test", "budget": 1234}' -H "Content-Type: application/json" -X POST http://localhost:5000/api/accounts/
+
+// curl -X PUT -H "Content-Type: application/json" -d '{"name":"test2"}' "http://localhost:5000/api/accounts/"
--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,23 +1,69 @@
-const router = require('express').Router()
+const router = require('express').Router();
+// The ABOVE lines combines the BELOW two lines.
+// WHAT:  bring in express, set the router
+// const express = require('express')
+// const router = express.Router()
 
-router.get('/', (req, res, next) => {
-  // DO YOUR MAGIC
+
+const Account = require('./accounts-model');
+
+router.get('/', async (req, res, next) => {
+  try {
+    const accounts = await Account.getAll() 
+    console.log("accounts: ", accounts)
+    res.status(200).json(accounts)
+  } catch (err) {
+    next(err)
+  }
 })
 
-router.get('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+router.get('/:id', async (req, res, next) => {
+  const {id} = req.params;
+  try {
+    const accountById = await Account.getById(id)
+    console.log("accountById: ", accountById);
+    res.status(200).json(accountById);
+  } catch (err) {
+    next(err)
+  }
 })
 
-router.post('/', (req, res, next) => {
-  // DO YOUR MAGIC
+router.post('/', async (req, res, next) => {
+  const newAccount = req.body;
+  console.log("newAccount: ", newAccount);
+  try {
+    const postedAccount = await Account.create(newAccount)
+    console.log("postedAccount: ", postedAccount);
+    res.status(201).json(postedAccount);
+  } catch (err) {
+    console.log("Error in create: ", err)
+    next(err)
+  }
 })
 
-router.put('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+router.put('/:id', async (req, res, next) => {
+  const id = req.params.id;
+  const changes = req.body;
+  try {
+    const updatedAccount = await Account.updateById(id, changes);
+    console.log("updatedAccount: ", updatedAccount);
+    res.status(200).json(updatedAccount);
+  } catch (err) {
+    console.log("Error in updateById: ", err)
+    next(err)
+  }
 });
 
-router.delete('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+router.delete('/:id', async (req, res, next) => {
+  const id = req.params.id;
+  try {
+    const deletedAccount = await Account.deleteById(id);
+    console.log("deletedAccount: ", deletedAccount);
+    res.status(200).json(deletedAccount);
+  } catch (err) {
+    console.log("Error in deleteById: ", err)
+    next(err)
+  }
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
