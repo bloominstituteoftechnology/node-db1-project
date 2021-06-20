3c75aea1cd364b6a36a1fc7c117409d5f7980180
orlando taylor <ackerie_taylor@hotmail.com>
2021-06-19 22:32:57 -0400

    work

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,21 +1,32 @@
 const getAll = () => {
   // DO YOUR MAGIC
+  return db('account')
 }
 
 const getById = id => {
   // DO YOUR MAGIC
+  return db('accounts').where('id', id).first()
 }
 
-const create = account => {
+const create = async (accounts) => {
   // DO YOUR MAGIC
+ const {id}= await db('account').insert({name, budget})
+  const {name, budget}=accounts
+  return getById
 }
 
 const updateById = (id, account) => {
   // DO YOUR MAGIC
+  id = Number(id)
+  await db('account').where('id', id).update({name, budget})
+  return getById(id)
 }
 
 const deleteById = id => {
   // DO YOUR MAGIC
+  const deletedAccount = await getById(id)
+  await db('account').where('id', id).delete()
+  return deletedAccount
 }
 
 module.exports = {
--- api/server.js
+++ api/server.js
@@ -1,7 +1,12 @@
 const express = require("express");
-
+const AccountRouter = require('./accounts/accounts-router')
 const server = express();
 
 server.use(express.json());
+server.use('/apiu/accounts', AccountRouter)
+
+server.get('/', (req, res) => {
+    res.status(200).json('Api up and running')
+})
 
 module.exports = server;
