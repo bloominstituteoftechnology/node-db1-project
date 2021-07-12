32c3caee86a72ed7cdc03f3aa4f6fcf6482e7b9e
wzd200 <william.z.dye@gmail.com>
2021-07-12 16:01:04 -0400

    Added error handling middleware and linked up router

--- api/server.js
+++ api/server.js
@@ -1,7 +1,28 @@
-const express = require("express");
+const express = require("express")
 
-const server = express();
+const server = express()
 
-server.use(express.json());
+const accountsRouter = require('./accounts/accounts-router')
 
-module.exports = server;
+const notFound = (req, res, next) => { // eslint-disable-line
+    res.status(404).json({
+        message: 'Not found, sorry!'
+    })
+}
+
+const errorHandling = (err, req, res, next) => { // eslint-disable-line
+    const status = err.status || 500
+    res.status(status).json({
+        message: err.message,
+    })
+}
+
+server.use(express.json())
+
+server.use('/api/accounts', accountsRouter)
+
+server.use('*', notFound)
+
+server.use(errorHandling)
+
+module.exports = server
