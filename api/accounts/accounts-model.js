42511bda91cfb9fc82091a85d4f9eed523449990
miketillman95 <matillman95@gmail.com>
2021-08-18 20:45:27 -0700

    hmwk

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,20 +1,38 @@
+const db = require('../../data/db-config')
+
+
+// R
 const getAll = () => {
+
+  return db('accounts')
   // DO YOUR MAGIC
 }
-
+//  R specific
 const getById = id => {
+  return db('accounts').where('id', id).first()
   // DO YOUR MAGIC
 }
+// C
+ const create = async (account) => {
+  const[id] = await db('accounts').insert(account)
+  
+return(getById)
 
-const create = account => {
   // DO YOUR MAGIC
 }
-
-const updateById = (id, account) => {
+// U
+ const updateById = async (id, account) => {
+  await db('accounts').where('id', id).update(id, account)
+return updateById(id)
   // DO YOUR MAGIC
 }
+// D
+async const deleteById = async (id) => {
+  const deletedAccount = await getById(id)
+  await db('account').where('id', id).delete()
 
-const deleteById = id => {
+  return deletedAccount
+
   // DO YOUR MAGIC
 }
 
