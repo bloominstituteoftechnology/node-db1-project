f6f9ccbe45f74ad2a0aad05ca2f67c917ae90c11
Joey Bertschler <joey.bertschler@gmail.com>
2021-09-13 19:33:13 +0200

    WIP

--- api/server.js
+++ api/server.js
@@ -4,4 +4,11 @@ const server = express();
 
 server.use(express.json());
 
+server.use('*', (req, res) => {
+    console.log('this is the catch-all fallback')
+    res.status(404).json({
+        message: 'not found'
+    })
+})
+
 module.exports = server;
