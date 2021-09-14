caeed705f983a4c2ffe3aa511693b495e46b281d
Joey Bertschler <joey.bertschler@gmail.com>
2021-09-14 04:53:21 +0200

    endpoints wired and working

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -2,26 +2,63 @@ const router = require('express').Router()
 
 router.get('/', (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+    res.json('get accounts')
+    res.json( [ {}, {lol:'lol'}, {}])
+    //throw new Error('kabloinZZZGGZ')
+  } catch (err) {
+    next(err)
+    //or
+    // next( {status: 422, message: "this is bad john, we errored"} )
+  }
 })
 
 router.get('/:id', (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+    res.json('get account by id')
+
+  } catch (err) {
+    next(err)
+  }
 })
 
 router.post('/', (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+    res.json('post account')
+
+  } catch (err) {
+    next(err)
+  }
 })
 
 router.put('/:id', (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+    res.json('update account by id')
+
+  } catch (err) {
+    next(err)
+  }
 });
 
 router.delete('/:id', (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+    res.json('delete account')
+
+  } catch (err) {
+    next(err)
+  }
 })
 
+//this is our error handling middleware all the     catch(err){next(err)}   point :
 router.use((err, req, res, next) => { // eslint-disable-line
   // DO YOUR MAGIC
+  res.status(err.status || 500).json({
+    message: err.message,
+  })
 })
 
 module.exports = router;
