19e0a6480f30ca06c55dbdbdb3f9a5eda6b63d2d
Joey Bertschler <joey.bertschler@gmail.com>
2021-09-15 16:48:26 +0200

    check unique account middleware done

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,4 +1,5 @@
 const Account = require('./accounts-model')
+const db =require('../../data/db-config')
 
 
 exports.checkAccountPayload = (req, res, next) => {
@@ -8,27 +9,40 @@ exports.checkAccountPayload = (req, res, next) => {
   const {name, budget} = req.body
   if (name === undefined || budget === undefined) {
     error.message = "name and budget are required"
-    next(error)
   } else if (typeof name !== 'string') {
     error.message = "name of account must be a string"
-    next(error)
   } else if (name.trim().length < 3 || name.trim().length > 100) {
     error.message = "name of account must be between 3 and 100"
-    next(error)
   } else if (typeof budget !== 'number' || isNaN(budget)) {
     error.message = "budget of account must be a number"
-    next(error)
   } else if (budget < 0 || budget > 1000000) {
     error.message = "budget of account is too large or too small"
+  }
+
+  if (error.message) {
     next(error)
+  } else {
+    next()
   }
 }
 
 
-exports.checkAccountNameUnique = (req, res, next) => {
+exports.checkAccountNameUnique = async (req, res, next) => {
   // DO YOUR MAGIC
-  console.log('test 2')
-  next()
+  //console.log('test 2')
+  //next()
+  try {
+    const existing = await db('accounts').where('name', req.body.name.trim())
+    .first()
+
+    if(existing) {
+      next({ status: 400, message: 'that name is taken'})
+    } else {
+      next()
+    }
+  } catch(err) {
+    next(err)
+  }
 }
 
 exports.checkAccountId = async (req, res, next) => {
