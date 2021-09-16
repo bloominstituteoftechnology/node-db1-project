3a91859502fda617dfbf1f52e4ab81c2e03fd94b
nicholastorr <nick.torres213@gmail.com>
2021-09-15 23:31:52 -0400

    added middleware

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,21 +1,24 @@
+const db = require('../../data/seeds/02-accounts');
+
+
 const getAll = () => {
-  // DO YOUR MAGIC
+  db('accounts')
 }
 
 const getById = id => {
-  // DO YOUR MAGIC
+  db('accounts').where('id', id)
 }
 
 const create = account => {
-  // DO YOUR MAGIC
+   db('accounts').insert(account)
 }
 
 const updateById = (id, account) => {
-  // DO YOUR MAGIC
+  db('accounts').where('id', id).update(account)
 }
 
 const deleteById = id => {
-  // DO YOUR MAGIC
+  db('accounts').where('id', id).delete()
 }
 
 module.exports = {
--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 
 router.get('/', (req, res, next) => {
-  // DO YOUR MAGIC
+  res.send('Welcome Home')
 })
 
 router.get('/:id', (req, res, next) => {
