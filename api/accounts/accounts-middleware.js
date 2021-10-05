c5cbf13784f7555e8ae3001bbdb6b8e838b824db
sean-mahan <seanmahansean@gmail.com>
2021-10-05 04:38:44 -0500

    checkAccountPayload finished

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,10 +1,23 @@
 const Account = require("./accounts-model")
 
 exports.checkAccountPayload = (req, res, next) => {
-  try{
-    next()
-  }catch(err){
+  const {name, budget} = req.body
+  const err = {status: 400}
+  if(name === undefined || budget === undefined){
+    err.message = "name and budget are required"
+  }else if(typeof name !== "string"){
+    err.message = "name of account must be a string"
+  }else if(name.trim().length < 3 || name.trim().length > 100){
+    err.message = "name of account must be between 3 and 100"
+  }else if(typeof budget !== "number" || isNaN(budget)){
+    err.message = "budget of account must be a number"
+  }else if(budget < 0 || budget > 1000000){
+    err.message = "budget of account is too large or too small"
+  }
+  if(err.message){
     next(err)
+  }else{
+    next()
   }
 }
 
