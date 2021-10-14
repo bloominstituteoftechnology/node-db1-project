423b7906962505eae77f9f9b48308e13ecdf1929
unknown <mykael.barnes@hotmail.com>
2021-10-13 20:32:42 -0700

    Mykael-Barnes

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,21 +1,27 @@
-const getAll = () => {
-  // DO YOUR MAGIC
+const db = require("../../data/db-config.js")
+
+ const getAll = () => {
+  return db("accounts")
 }
 
-const getById = id => {
-  // DO YOUR MAGIC
+ const getById = id => {
+  return db("accounts").where("id",id).first()  
 }
 
-const create = account => {
-  // DO YOUR MAGIC
+ const create = async function (account){
+  const [id] = await db("accounts").insert(account)
+  return getById(id)
 }
 
-const updateById = (id, account) => {
-  // DO YOUR MAGIC
+ const updateById = async function (id, account){
+  await db("accounts").where("id",id).update(account)
+  return getById(id)
 }
 
-const deleteById = id => {
-  // DO YOUR MAGIC
+ const deleteById = async function (id){
+  const deletedAccount = await getById(id)
+  await db("accounts").where("id",id).delete()
+  return deletedAccount
 }
 
 module.exports = {
--- api/server.js
+++ api/server.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const accountRouter = require('./accounts/accounts-router')
 
 const server = express();
 
 server.use(express.json());
+server.use('/api/accounts', accountRouter)
 
 module.exports = server;
