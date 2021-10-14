0509876c28f5f02f47c320374f7646cd73cb981c
Gabe Chilson <g.chilson34@gmail.com>
2021-10-13 18:25:15 -0600

    finished MVP tasks

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,11 +1,55 @@
+const Account = require('./accounts-model')
+const db = require('../../data/db-config')
+
+
+
 exports.checkAccountPayload = (req, res, next) => {
-  // DO YOUR MAGIC
+  const error = { status: 400 }
+  const { name, budget } = req.body
+  if( name === undefined || budget === undefined ) {
+    error.message = 'name and budget are required'
+    next(error)
+  } else if(typeof name !== 'string') {
+    res.status(400).json({ message: 'name of account must be a string'})
+  } else if(name.trim().length < 3 || name.trim().length > 100) {
+    res.status(400).json({ message: 'name of account must be between 3 and 100'})
+  } else if(typeof budget !== 'number' || isNaN(budget)) {
+    res.status(400).json({ message: 'budget of account must be a number'})
+  } else if(budget < 0 || budget > 1000000) {
+    res.status(400).json({ message: 'budget of account is too large or too small'})
+  } 
+
+  if (error.message) {
+    next(error)
+  } else {
+    next()
+  }
 }
 
-exports.checkAccountNameUnique = (req, res, next) => {
-  // DO YOUR MAGIC
+exports.checkAccountNameUnique = async (req, res, next) => {
+  try {
+    const takenName = await db('accounts')
+      .where('name', req.body.name.trim())
+      .first()
+    if (takenName) {
+      next( { status: 400, message: 'that name is taken '})
+    } else {
+      next()
+    }
+  } catch (err) {
+    next(err)
+  }
 }
 
-exports.checkAccountId = (req, res, next) => {
-  // DO YOUR MAGIC
+exports.checkAccountId = async (req, res, next) => {
+  try {
+    const account = await Account.getById(req.params.id)
+    if(!account) {
+      next({ status: 404, message: 'account not found'})
+    } else {
+      req.account = account
+      next()    }
+  } catch (err) {
+    next(err)
+  }
 }
--- queries.sql
+++ queries.sql
@@ -1,16 +1,27 @@
 -- Database Queries
 
 -- Find all customers with postal code 1010
+SELECT * FROM [Customers]
+where PostalCode = '1010';
 
 -- Find the phone number for the supplier with the id 11
+SELECT Phone FROM [Suppliers] where SupplierID = '11';
 
 -- List first 10 orders placed, sorted descending by the order date
+select * from Orders
+limit 10;
 
 -- Find all customers that live in London, Madrid, or Brazil
+SELECT * FROM [Customers] where City = 'London' or 'Madrid' or Country = 'Brazil';
 
 -- Add a customer record for "The Shire", the contact name is "Bilbo Baggins" the address is -"1 Hobbit-Hole" in "Bag End", postal code "111" and the country is "Middle Earth"
+insert into Customers (Country, CustomerName, ContactName, Address, City, PostalCode)
+values ('Middle Earth', 'The Shire', 'Bilbo Baggins', '1 Hobbit-Hole', 'Bag End', '111');
 
 -- Update Bilbo Baggins record so that the postal code changes to "11122"
+update Customers
+set PostalCode = '11122'
+where CustomerName = 'Bilbo Baggins';
 
 -- (Stretch) Find a query to discover how many different cities are stored in the Customers table. Repeats should not be double counted
 
