8e0a2c4a1779ca07a41a5a073df587ea094e55dd
tamara-taylor <ttaylor8522@gmail.com>
2021-10-13 21:33:24 -0400

    still working on it

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,11 +1,51 @@
+const dbConfig = require('../../data/db-config')
+const Account = require('./accounts-model')
+
 exports.checkAccountPayload = (req, res, next) => {
-  // DO YOUR MAGIC
+  const err = {statua: 400}
+  const {name, budget} = req.body
+  if(name === undefined || budget === undefined){
+    err.message = 'name and budget are required'
+    next(err)
+  }else if(typeof name !== 'string'){
+    err.message = 'name of account must be a string'
+    next(err)
+  }else if(name.trim().length < 3 || name.trim().length > 100){
+  err.message = 'name of the account must be between 3 and 100'
+  next(err)
+  }else if(typeof budget !== 'number' || isNan(budget)){
+    err.message = 'budget of account must be a number'
+    next(err)
+  }else if(budget < 0 || budger >1000000){
+    err.message = 'budget of account is too large or too small'
+    next(err)
+  }
 }
 
-exports.checkAccountNameUnique = (req, res, next) => {
-  // DO YOUR MAGIC
+exports.checkAccountNameUnique = async (req, res, next) => {
+  try{
+    const existing = await db('accounts').where('name', req.body.name.trim())
+    .first()
+    if(existing){
+      next({status: 400, message: 'that name is taken'})
+    }else{
+      next()
+    }
+  }catch(err){
+    next(err)
+  }
 }
 
-exports.checkAccountId = (req, res, next) => {
-  // DO YOUR MAGIC
+exports.checkAccountId = async (req, res, next) => {
+  try{
+    const account = await Account.getById(req.params.id)
+    if(!account){
+      next({status: 404, message: 'account not found'})
+    }else{
+      req.account = account
+      next()
+    }
+  }catch(err){
+    next(err)
+  }
 }
--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,21 +1,23 @@
+const db = require('../../data/db-config')
+
 const getAll = () => {
-  // DO YOUR MAGIC
+  return db('accounts')
 }
 
 const getById = id => {
-  // DO YOUR MAGIC
+  return db('accounts').where('id', id)
 }
 
 const create = account => {
-  // DO YOUR MAGIC
+  return db('accounts').insert(account)
 }
 
 const updateById = (id, account) => {
-  // DO YOUR MAGIC
+  return db('accounts').where('id', id).update(account)
 }
 
 const deleteById = id => {
-  // DO YOUR MAGIC
+  return db('accounts').where('id', id).del()
 }
 
 module.exports = {
