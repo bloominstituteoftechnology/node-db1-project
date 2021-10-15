97cbbaeba469dc91d787163199048cdb634bd45a
unknown <j.ordonez419@gmail.com>
2021-10-14 20:44:57 -0500

    mvp complete

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,11 +1,58 @@
+const Account = require('./accounts-model')
+
 exports.checkAccountPayload = (req, res, next) => {
   // DO YOUR MAGIC
+  console.log('checking payload')
+  if (!req.body.name || !req.body.budget) {
+    res.status(400).json({ message: 'name and budget are required' })
+  }
+  else if (typeof req.body.name != 'string') {
+    res.status(400).json({ message: "name of account must be a string" })
+  }
+  else if (req.body.name.length < 3 || req.body.name.length > 100) {
+    res.status(400).json({ message: "name of account must be between 3 and 100" })
+  }
+  else if (typeof req.body.budget != 'number') {
+    res.status(400).json({ message: "budget of account must be a number" })
+  }
+  else if (req.body.budget < 0 || req.budget > 1000000) {
+    res.status(400).json({ message: "budget of account is too large or too small" })
+  }
+
+  else {
+    next()
+  }
 }
 
 exports.checkAccountNameUnique = (req, res, next) => {
   // DO YOUR MAGIC
+  db('accounts').where({ name: req.body.name })
+    .then(account => {
+      if (!account) {
+        next()
+      } else {
+        res.status(400).json('that name is taken')
+      }
+    })
+    .catch(err => {
+      res.json({ message: err.message })
+    })
 }
 
 exports.checkAccountId = (req, res, next) => {
   // DO YOUR MAGIC
+  console.log('checking account id')
+  const { id } = req.params;
+  Account.getById(id)
+    .then(account => {
+      if (!account) {
+        res.status(404).json({ message: "account not found" })
+      } else {
+        next()
+      }
+    })
+    .catch(err => {
+      res.statu(500).json({ message: 'error retrieving from database' })
+    })
+
 }
--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,21 +1,32 @@
+const db = require('../../data/db-config')
+
 const getAll = () => {
   // DO YOUR MAGIC
+  return db('accounts')
 }
 
-const getById = id => {
+const getById = idParam => {
   // DO YOUR MAGIC
+  return db('accounts').where('id', idParam).first()
+  // Get all accounts where the id column, is equal to the id passed in
 }
 
-const create = account => {
+const create = async ({ name, budget }) => {
   // DO YOUR MAGIC
+  const [id] = await db('accounts').insert({ name, budget })
+  return getById(id)
 }
 
-const updateById = (id, account) => {
+const updateById = async (id, { name, budget }) => {
   // DO YOUR MAGIC
+  await db("accounts").where("id", id).update({ name, budget })
+  return getById(id)
 }
 
-const deleteById = id => {
-  // DO YOUR MAGIC
+const deleteById = async (id) => {
+  const deletedPost = await getById(id)
+  await db("accounts").where("id", id).delete()
+  return deletedPost
 }
 
 module.exports = {
