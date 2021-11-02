be2643a053ccca2f0828ec485b72f9018e9c9b82
Abraman18 <abraman18@gmail.com>
2021-11-02 05:03:41 -0700

    fixed treacherous budget is not a number bug

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -5,7 +5,7 @@ function checkAccountPayload (req, res, next) {
   let { name, budget } = req.body;
   // name = name.trim();
   
-  if (!name || !budget) {
+  if (name === undefined || budget === undefined) {
     res.status(400).json({ message: 'name and budget are required' })
   }
   else if (typeof(name) != 'string') {
@@ -14,7 +14,7 @@ function checkAccountPayload (req, res, next) {
   else if (name.trim().length < 3 || name.trim().length > 100) {
     res.status(400).json({ message: "name of account must be between 3 and 100" })
   }
-  else if (isNaN(budget)) {
+  else if (typeof budget !== 'number' || isNaN(budget)) {
     res.status(400).json({ message: "budget of account must be a number" })
   }
   else if (budget <= 1 || budget > 1000000) {
