4c2ab49389fce600ef1a82983a92d7452588bd80
Cynthia Coronado <cynthia.softball12@gmail.com>
2021-11-10 07:48:39 -0800

    db access functions complete

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,21 +1,39 @@
+const db = require('../../data/db-config')
+
 const getAll = () => {
   // DO YOUR MAGIC
+  return db('accounts')
 }
 
-const getById = id => {
+const getById = async (id) => {
   // DO YOUR MAGIC
+  const result  = await db('accounts')
+    .where('id', id).first()
+  return result
 }
 
-const create = account => {
+const create = async (account) => {
   // DO YOUR MAGIC
+  const [ id ] = await db('accounts')
+   .insert(account)
+  return getById(id)
 }
 
-const updateById = (id, account) => {
+const updateById = async (id, account) => {
   // DO YOUR MAGIC
+  await db('accounts')
+   .where('id', id)
+   .update(account)
+  return getById(id)
 }
 
-const deleteById = id => {
+const deleteById = async (id) => {
   // DO YOUR MAGIC
+  const delectAccount = await getById(id) 
+  await db('accounts')
+    .where({ id })
+    .del()
+  return delectAccount
 }
 
 module.exports = {
