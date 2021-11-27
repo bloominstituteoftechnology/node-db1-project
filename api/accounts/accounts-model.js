5dd8e9f15b02f77d0a5d3dafec3417314b3212fe
Cynthia Coronado <cynthia.softball12@gmail.com>
2021-11-27 14:00:59 -0800

    endpoints connected

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -2,38 +2,27 @@ const db = require('../../data/db-config')
 
 const getAll = () => {
   // DO YOUR MAGIC
-  return db('accounts')
+  
 }
 
 const getById = async (id) => {
   // DO YOUR MAGIC
-  const result  = await db('accounts')
-    .where('id', id).first()
-  return result
+  
 }
 
 const create = async (account) => {
   // DO YOUR MAGIC
-  const [ id ] = await db('accounts')
-   .insert(account)
-  return getById(id)
+  
 }
 
 const updateById = async (id, account) => {
   // DO YOUR MAGIC
-  await db('accounts')
-   .where('id', id)
-   .update(account)
-  return getById(id)
+  
 }
 
 const deleteById = async (id) => {
   // DO YOUR MAGIC
-  const delectAccount = await getById(id) 
-  await db('accounts')
-    .where({ id })
-    .del()
-  return delectAccount
+   
 }
 
 module.exports = {
--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,52 +1,50 @@
 const router = require('express').Router()
+const md = require('./accounts-middleware')
 
-const Account = require('./accounts-model')
-const md = require('./accounts-middleware.js');
-
-
-router.get('/', async (req, res, next) => {
-  try {
-    const data = await Account.getAll()
-    res.json(data)
-  } catch (err) {
+router.get('/', (req, res, next) => {
+  try{
+    res.json('get accounts')
+  }catch(err){
     next(err)
   }
 })
 
-router.get('/:id', md.checkAccountId, 
-  async (req, res, next) => {
-  res.json(req.account)
-})
-
-router.post('/', md.checkAccountPayload, md.checkAccountNameUnique, async (req, res, next) => {
-  try {
-    const data = await Account.create(req.body)
-    res.status(201).json(data)
-  } catch (err) {
+router.get('/:id', (req, res, next) => {
+  try{
+res.json('get account by id')
+  }catch(err){
     next(err)
   }
 })
 
-router.put('/:id', md.checkAccountId, md.checkAccountPayload, async (req, res, next) => {
-  try {
-    const data = await Account.updateById(req.params.id, req.body)
-    res.json(data)
-  } catch (err) {
+router.post('/', (req, res, next) => {
+  try{
+res.json('post account')
+  }catch(err){
+    next(err)
+  }
+})
+
+router.put('/:id', (req, res, next) => {
+  try{
+res.json('update account')
+  }catch(err){
     next(err)
   }
 });
 
-router.delete('/:id', md.checkAccountId, async (req, res, next) => {
-  try {
-    const data = await Account.deleteById(req.params.id)
-    res.json(data)
-  } catch (err) {
+router.delete('/:id', (req, res, next) => {
+  try{
+res.json('delets accounts')
+  }catch(err){
     next(err)
   }
 })
 
-router.use((err, req, res, next) => {
-  res.status(err.status || 500).json({ message: err.message,})
+router.use((err, req, res, next) => { // eslint-disable-line
+  res.status(err.status || 500).json({
+    message: err.message
+  })  
 })
 
 module.exports = router;
