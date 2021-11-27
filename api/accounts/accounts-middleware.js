f8ca82fdb0eb2c8258ba6c5fb9ad97838c33c0aa
Cynthia Coronado <cynthia.softball12@gmail.com>
2021-11-27 14:50:35 -0800

    moved error message in checkAccountPayload

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -7,19 +7,19 @@ exports.checkAccountPayload = (req, res, next) => {
   const{ name, budget } = req.body
   if( name === undefined || budget === undefined ) {
     error.message = 'name and budget are required'
-    next(error)
   }else if (typeof name !== 'string') {
     error.message = 'name of account must be a string'
-    next(error)
   }else if (name.trim().length < 3 || name.trim().length> 100){
     error.message = 'name of account must be between 3 and 100'
-    next(error)
   }else if (typeof budget !== 'number' || !isNaN(budget)){
     error.message = 'budget of account must be a number'
-    next(error)
   }else if (budget < 0 || budget > 1000000) {
     error.message = 'budget of account is too large or too small'
+  }
+  if(error.message){
     next(error)
+  }else{
+    next()
   }
 }
 
