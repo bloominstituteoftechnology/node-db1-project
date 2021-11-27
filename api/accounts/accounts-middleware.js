3de6aa8665d8737966f00df8f47917a589879570
Cynthia Coronado <cynthia.softball12@gmail.com>
2021-11-27 14:35:45 -0800

    checkAccountPayload completed

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -2,8 +2,25 @@ const Account = require('./accounts-model')
 const db = require('../../data/db-config')
 
 exports.checkAccountPayload = (req, res, next) => {
-  console.log('checkAccountPayload md');
-  next()
+  // console.log('checkAccountPayload md');
+  const error = { status: 400 }
+  const{ name, budget } = req.body
+  if( name === undefined || budget === undefined ) {
+    error.message = 'name and budget are required'
+    next(error)
+  }else if (typeof name !== 'string') {
+    error.message = 'name of account must be a string'
+    next(error)
+  }else if (name.trim().length < 3 || name.trim().length> 100){
+    error.message = 'name of account must be between 3 and 100'
+    next(error)
+  }else if (typeof budget !== 'number' || !isNaN(budget)){
+    error.message = 'budget of account must be a number'
+    next(error)
+  }else if (budget < 0 || budget > 1000000) {
+    error.message = 'budget of account is too large or too small'
+    next(error)
+  }
 }
 
 exports.checkAccountNameUnique = (req, res, next) => {
