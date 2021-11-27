87a75aa0af4f95b829f7bd0b97eeac882f0c5205
Cynthia Coronado <cynthia.softball12@gmail.com>
2021-11-27 14:16:41 -0800

    working on model and router

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,12 +1,13 @@
 const db = require('../../data/db-config')
 
 const getAll = () => {
-  // DO YOUR MAGIC
-  
+  //select * from accounts
+  return db('accounts')
 }
 
 const getById = async (id) => {
-  // DO YOUR MAGIC
+  //select * from accounts where id = 1
+  return db('accounts').where('id', id).first()
   
 }
 
--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,17 +1,20 @@
 const router = require('express').Router()
 const md = require('./accounts-middleware')
+const Account = require('./accounts-model')
 
-router.get('/', (req, res, next) => {
+router.get('/', async (req, res, next) => {
   try{
-    res.json('get accounts')
+    const accounts = await Account.getAll()
+    res.json(accounts)
   }catch(err){
     next(err)
   }
 })
 
-router.get('/:id', md.checkAccountId, (req, res, next) => {
+router.get('/:id', md.checkAccountId, async (req, res, next) => {
   try{
-res.json('get account by id')
+    const account = await Account.getById(req.params.id)
+    res.json(account)
   }catch(err){
     next(err)
   }
