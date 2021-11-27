13249e1b8a40249fb0996f99e440b4cf8feef81a
Cynthia Coronado <cynthia.softball12@gmail.com>
2021-11-27 13:33:17 -0800

    starting over, server up and running

--- api/server.js
+++ api/server.js
@@ -4,4 +4,10 @@ const server = express();
 
 server.use(express.json());
 
+server.use('*', (req, res) => {
+    res.status(404).json({
+        message: 'not found'
+    })
+})
+
 module.exports = server;
