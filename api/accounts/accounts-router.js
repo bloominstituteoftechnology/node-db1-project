8c9c5d214dd7891271a8e802d7a2aa3d32ea5d30
joey-bertschler <joey.bertschler@gmail.com>
2021-12-14 14:57:02 +0400

    endpoint wired and working

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -2,26 +2,56 @@ const router = require('express').Router()
 
 router.get('/', (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+    res.json(['get accounts',{},{},{}])
+  } catch (err) {
+    next(err)
+    //next({status:422, message: 'test error next'})
+    //422 = unprocessable entity
+  }
 })
 
 router.get('/:id', (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+    res.json('get account by id')
+  } catch (err) {
+    next(err)
+  }
 })
 
 router.post('/', (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+    res.json('post account')
+  } catch (err) {
+    next(err)
+  }
 })
 
 router.put('/:id', (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+    res.json('update account')
+  } catch (err) {
+    next(err)
+  }
 });
 
 router.delete('/:id', (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+    res.json('delete account')
+  } catch (err) {
+    next(err)
+  }
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
   // DO YOUR MAGIC
-})
-
+  res.status(err.status || 500).json({
+    message: err.message,
+  })
+})  
+ 
 module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 
+const accountsRouter = require("./accounts/accounts-router.js");
+
 const server = express();
 
 server.use(express.json());
 
+server.use("/api/accounts", accountsRouter);
 
 server.use('*', (req, res) => {
     res.status(404).json({
