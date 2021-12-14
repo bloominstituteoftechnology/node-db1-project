779ac892f263dac38f6db7089f33c2a75cc58816
joey-bertschler <joey.bertschler@gmail.com>
2021-12-14 14:05:55 +0400

    wip

--- api/server.js
+++ api/server.js
@@ -4,4 +4,12 @@ const server = express();
 
 server.use(express.json());
 
+
+server.use('*', (req, res) => {
+    res.status(404).json({
+        message: 'Error 404: Page not found',
+        //error: err,'Not found'
+    })
+})
+
 module.exports = server;
