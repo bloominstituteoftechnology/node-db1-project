c7fc76af95ab732e9861751e3ac5faf00b76983c
joey-bertschler <joey.bertschler@gmail.com>
2021-12-15 04:22:57 +0400

    middleware payload checker

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,13 +1,38 @@
 //const db = require('../../data/db-config');
 const Account = require('../accounts/accounts-model.js');
 
+//find bug, cant start debugger
+
 exports.checkAccountPayload = (req, res, next) => {
-  // DO YOUR MAGIC
-  console.log('checkAccountPayload middleware')
-  next()
-  // Note: you can either write "manual" validation logic
-  // or use the Yup library (not currently installed)
-}
+  const error = {status: 400}
+  
+  //const {name, budget} = req.body;
+  const name = req.body.name
+  const budget = req.body.budget
+
+ // if (!name || !budget || name === undefined || budget === undefined) {
+  if (name === undefined || budget === undefined) {
+    error.message = 'name and budget are required' //Missing name or budget'
+    next(error)
+  
+  
+  } else if (typeof name !== 'string') {
+    error.message = 'name of account must be a string'
+    next(error)
+  } else if (name.trim().length < 3 || name.trim().length > 100) {
+      error.message = 'name of account must be between 3 and 100 characters'
+    next(error)
+    //when checking type in budget:= not budget =
+  } else if (typeof budget !== 'number' || isNaN(budget)) {
+    error.message = 'budget of account must be a number'
+    console.log(budget)
+    next(error)
+  } else if (budget < 0 || budget > 1000000) {
+    error.message = 'budget of account is too large or too small'
+    next(error)
+  }
+} 
+
 
 exports.checkAccountNameUnique = (req, res, next) => {
   // DO YOUR MAGIC
