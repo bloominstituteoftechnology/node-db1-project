0d2aa4069f0f730a77c08811549722651574c4bd
Alieze-Ali <71855821+Alieze-Ali@users.noreply.github.com>
2021-12-18 15:42:59 -0800

    added test api to server

--- api/server.js
+++ api/server.js
@@ -4,4 +4,9 @@ const server = express();
 
 server.use(express.json());
 
+server.use('*', (req, res) => {
+    res.status(404).json({
+        message: 'not found',
+    })
+})
 module.exports = server;
