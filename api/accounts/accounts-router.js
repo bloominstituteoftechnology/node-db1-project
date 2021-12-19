8488279752d71bbb36e4b5306db28d610cb5d0d4
Alieze-Ali <71855821+Alieze-Ali@users.noreply.github.com>
2021-12-18 16:25:02 -0800

    basic endpoint testing

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -2,26 +2,55 @@ const router = require('express').Router()
 
 router.get('/', (req, res, next) => {
   // DO YOUR MAGIC
+  try{
+    //throw new Error('yikes!!!')
+    res.json("get account")
+  } catch(err)  {
+    next(err)
+  }
 })
 
 router.get('/:id', (req, res, next) => {
   // DO YOUR MAGIC
+  try{
+    res.json("get account id")
+  } catch(err)  {
+    next(err)
+  }
 })
 
 router.post('/', (req, res, next) => {
   // DO YOUR MAGIC
+  try{
+    res.json("post account")
+  } catch(err)  {
+    next(err)
+  }
 })
 
 router.put('/:id', (req, res, next) => {
   // DO YOUR MAGIC
+  try{
+    res.json("update account")
+  } catch(err)  {
+    next(err)
+  }
 });
 
 router.delete('/:id', (req, res, next) => {
   // DO YOUR MAGIC
+  try{
+    res.json("delete account")
+  } catch(err)  {
+    next(err)
+  }
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
   // DO YOUR MAGIC
+  res.status(err.status || 500).json({
+    message: err.message,
+  })
 })
 
 module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,9 +1,14 @@
 const express = require("express");
+// consume the router  
+const accountsRouter = require('./accounts/accounts-router')
 
 const server = express();
 
 server.use(express.json());
+// connect router exactly here
+server.use('/api/accounts', accountsRouter);
 
+// Test api
 server.use('*', (req, res) => {
     res.status(404).json({
         message: 'not found',
