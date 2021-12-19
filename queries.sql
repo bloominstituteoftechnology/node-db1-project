b6ba2f6a269d2dc018209cdd3bc9fcffca6fc95e
Purefallen11 <53922216+Purefallen11@users.noreply.github.com>
2021-12-19 17:19:13 -0600

    done with sql queries, excluding the stretch

--- queries.sql
+++ queries.sql
@@ -17,9 +17,15 @@ SELECT * FROM [Customers]
 WHERE Country = "Brazil" 
 OR City = "Madrid"
 OR City = "London"
+
 -- Add a customer record for "The Shire", the contact name is "Bilbo Baggins" the address is -"1 Hobbit-Hole" in "Bag End", postal code "111" and the country is "Middle Earth"
+INSERT INTO [Customers](CustomerName, ContactName, Address, City, PostalCode, Country)
+VALUES ("The Shire", "Bilbo Baggins", "1 Hobbit-Hole", "Bag End", "111", "Middle Earth")
 
 -- Update Bilbo Baggins record so that the postal code changes to "11122"
+UPDATE [Customers]
+SET PostalCode = 11122
+WHERE PostcalCode = 111
 
 -- (Stretch) Find a query to discover how many different cities are stored in the Customers table. Repeats should not be double counted
 
