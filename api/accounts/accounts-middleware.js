7fd40498ad2614fb90a5186113f4722f0530be45
Alieze-Ali <71855821+Alieze-Ali@users.noreply.github.com>
2021-12-19 11:39:28 -0800

    got some of my  post and put working

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,12 +1,31 @@
 // Bring in accounts-model
 const Account = require('./accounts-model')
 
+// This api is not working correctly?????
 exports.checkAccountPayload = (req, res, next) => {
   // DO YOUR MAGIC
   // Note: you can either write "manual" validation logic
   // or use the Yup library (not currently installed)
-  console.log('checkAccountPayload middleware')
-  next()
+  // console.log('checkAccountPayload middleware')
+  const error = { status: 400 }
+  const { name, budget } = req.body
+  if (name === undefined || budget === undefined){
+    error.message = 'name and budget are required'
+    next(error)
+  } else if (typeof name !== 'string') {
+    error.message = 'name of account must be a string'
+    next(error)
+  } else if (name.trim().length < 3 || name.trim().length > 100) {
+    error.message = 'name of account must be between 3 and 100'
+    next(error)
+  } else if (typeof budget !== 'number' || isNaN(budget)) {
+    error.message = 'budget of account must be a number'
+    next(error)
+  } else if (budget < 0 || budget > 1000000) {
+    error.message = 'budget of account is too large or too small'
+    next(error)
+  }
+  
 }
 
 exports.checkAccountNameUnique = (req, res, next) => {
