b3b8fd1a6225048e26e8f2e075d0e79c27a45ff4
Purefallen11 <53922216+Purefallen11@users.noreply.github.com>
2021-12-19 18:03:09 -0600

    done with model, i hope i did it right, not sure tho;

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,21 +1,26 @@
+const db = require("../../data/db-config")
+
 const getAll = () => {
-  // DO YOUR MAGIC
+  return db("accounts")
 }
 
 const getById = id => {
-  // DO YOUR MAGIC
+  return db("accounts").where("id", id).first()
 }
 
 const create = account => {
-  // DO YOUR MAGIC
+  const [id] = db("accounts").insert(account)
+  return getById(id)
 }
 
 const updateById = (id, account) => {
-  // DO YOUR MAGIC
+  const [updatedId] = db("accounts").where("id", id).update(account)
+  return getById(updatedId)
 }
 
 const deleteById = id => {
-  // DO YOUR MAGIC
+  const [DeleteId] = db("accounts").where("id", id).del()
+  return DeleteId
 }
 
 module.exports = {
