92ce1a2b1f7a0008a9b5ee24f0285b6db2cede0f
Purefallen11 <53922216+Purefallen11@users.noreply.github.com>
2021-12-21 22:23:30 -0600

    still a bit iffy about what is honestly going on, but I think im getting it

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,13 +1,24 @@
+const Accounts = require("../accounts/accounts-model")
+
 exports.checkAccountPayload = (req, res, next) => {
-  // DO YOUR MAGIC
-  // Note: you can either write "manual" validation logic
-  // or use the Yup library (not currently installed)
+  
 }
 
 exports.checkAccountNameUnique = (req, res, next) => {
   // DO YOUR MAGIC
 }
 
-exports.checkAccountId = (req, res, next) => {
-  // DO YOUR MAGIC
+exports.checkAccountId = async (req, res, next) => {
+  try {
+    const { id } = req.params
+    const account = await Accounts.getById(id)
+    if (!account) {
+      res.status(404).json({message: `account with id ${id} does not exist`})
+    } else {
+      req.account = account
+      next()
+    }
+  } catch (err) {
+    res.status(500).json({message: err.message})
+  }
 }
--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,11 +1,19 @@
 const router = require('express').Router()
+const Accounts = require("./accounts-model")
+const {checkAccountId} = require("../accounts/accounts-middleware")
 
-router.get('/', (req, res, next) => {
-  // DO YOUR MAGIC
+router.get('/', async (req, res, next) => {
+  try {
+    const account = await Accounts.getAll()
+    res.status(200).json(account)
+  } catch (err) {
+    res.status(500).json({message: err.message})
+  }
+  next()
 })
 
-router.get('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+router.get('/:id',checkAccountId, (req, res, next) => {
+  res.status(200).json(req.account)
 })
 
 router.post('/', (req, res, next) => {
