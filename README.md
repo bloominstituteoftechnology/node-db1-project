7d5191ffe8567aebfa1766ec7bb21e80b32348d9
Purefallen11 <53922216+Purefallen11@users.noreply.github.com>
2021-12-21 23:25:17 -0600

    project done

--- README.md
+++ README.md
@@ -72,8 +72,11 @@ db('foo-table').where('id', id).delete() // resolves to the **number of records*
   - `checkAccountPayload` returns a status 400 with if `req.body` is invalid:
 
     - If either name or budget are undefined, return `{ message: "name and budget are required" }`
+    //
     - If the _trimmed_ name is shorter than 3 or longer than 100, return `{ message: "name of account must be between 3 and 100" }`
+    //
     - If budget is not able to be converted into a number, return `{ message: "budget of account must be a number" }`
+    //
     - If budget is a negative number or over one million, return  `{ message: "budget of account is too large or too small" }`
 
   - `checkAccountId` returns a status 404 with a `{ message: "account not found" }` if `req.params.id` does not exist in the database
--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 const Accounts = require("./accounts-model")
-const {checkAccountId} = require("../accounts/accounts-middleware")
+const {checkAccountId, checkAccountPayload, checkAccountNameUnique} = require("../accounts/accounts-middleware")
 
 router.get('/', async (req, res, next) => {
   try {
@@ -14,18 +14,31 @@ router.get('/', async (req, res, next) => {
 
 router.get('/:id',checkAccountId, (req, res, next) => {
   res.status(200).json(req.account)
+  next()
 })
 
-router.post('/', (req, res, next) => {
-  // DO YOUR MAGIC
+router.post('/', checkAccountPayload, checkAccountNameUnique, async (req, res, next) => {
+  const newAccount = await Accounts.create(req.body)
+  res.status(201).json(newAccount)
+  next()
 })
 
-router.put('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+router.put('/:id', checkAccountId, checkAccountPayload, async (req, res, next) => {
+  const { id } = req.params
+  const { body } = req.body
+  
+  const updatedAccount = await Accounts.updateById(id, body)
+  res.status(200).json(updatedAccount)
+  next()
 });
 
-router.delete('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+router.delete('/:id', checkAccountId, async (req, res, next) => {
+  const { id } = req.params
+  
+  const deletedAccount = await Accounts.deleteById(id)
+  res.status(200).json(deletedAccount)
+  
+  next()
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
