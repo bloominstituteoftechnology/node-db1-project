b24a6fcf7ee326fd5ab560fab1deb1e8620d7a34
Cynthia Coronado <cynthia.softball12@gmail.com>
2022-01-26 19:44:54 -0800

    checkAccountPayload middleware completed

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,7 +1,20 @@
+const Accounts = require('./accounts-model')
+const db = require('../../data/db-config')
+
 exports.checkAccountPayload = (req, res, next) => {
-  // DO YOUR MAGIC
-  // Note: you can either write "manual" validation logic
-  // or use the Yup library (not currently installed)
+  const { name, budget } = req.body
+  if(name === undefined || budget === undefined){
+    res.status(400).json({message: 'name and budget are required'})
+  }
+  else if(!name.trim().length < 3 || name.trim().length > 100){
+    res.status(400).json({message: 'name of account must be between 3 and 100'})
+  }
+  else if(typeof budget !== 'number' || NaN(budget)){
+    res.status(400).json({message: 'budget of account must be a number'})
+  }
+  else if(budget < 0 || budget > 1000000){
+    res.status(400).json({message: 'budget of account is too large or too small'})
+  }
 }
 
 exports.checkAccountNameUnique = (req, res, next) => {
