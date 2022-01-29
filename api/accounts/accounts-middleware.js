ab7c00fc37fa70a9b173fadf050547d3ba75dc98
Cynthia Coronado <cynthia.softball12@gmail.com>
2022-01-29 10:36:47 -0800

    not sure what is going on!

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -2,38 +2,55 @@ const Accounts = require('./accounts-model')
 const db = require('../../data/db-config')
 
 exports.checkAccountPayload = (req, res, next) => {
-  const { name, budget } = req.body
-  if(name === undefined || budget === undefined){
-    res.status(400).json({message: 'name and budget are required'})
+  const error = { status: 400 }
+  const{ name, budget } = req.body
+  if( name === undefined || budget === undefined ) {
+    error.message = 'name and budget are required'
+  }else if (name.trim().length < 3 || name.trim().length > 100){
+    error.message = 'name of account must be between 3 and 100'
+  }else if (typeof budget !== 'number' || !isNaN(budget)){
+    error.message = 'budget of account must be a number'
+  }else if (budget < 0 || budget > 1000000) {
+    error.message = 'budget of account is too large or too small'
   }
-  else if(!name.trim().length < 3 || name.trim().length > 100){
-    res.status(400).json({message: 'name of account must be between 3 and 100'})
-  }
-  else if(typeof budget !== 'number' || NaN(budget)){
-    res.status(400).json({message: 'budget of account must be a number'})
-  }
-  else if(budget < 0 || budget > 1000000){
-    res.status(400).json({message: 'budget of account is too large or too small'})
-  }
-}
-
-exports.checkAccountNameUnique = (req, res, next) => {
-  const takenName = db('accounts')
-  .where('name', req.body.name.trim())
-  .first()
-  if(takenName){
-    next({status: 400, message: 'that name is taken'})
+  if(error.message){
+    next(error)
   }else{
     next()
   }
 }
+
+exports.checkAccountNameUnique = async (req, res, next) => {
+  try{
+    const existing = await db('accounts')
+      .where('name', req.body.name.trim())
+      .first()
+      if(!existing){
+        res.status(400).json({
+          message: 'that name is taken'
+        })
+      }
+  }catch(err){
+    next(err)
+  }
+}
+
 
 exports.checkAccountId = (req, res, next) => {
-  const account = Accounts.getById(req.params.id)
-  if(!account){
-    res.status(404).json({message: 'that name is taken'})
-  }else{
-    req.account = account
-    next()
-  }
+  const { id } = req.params
+  Accounts.getById(id)
+    .then(account => {
+      if(!account){
+        res.status(404).json({
+          message: 'account not found'
+        })
+      }else{
+        next()
+      }
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: 'error retireving from database'
+      })
+    })
 }
--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,33 +1,30 @@
 const db = require('../../data/db-config')
 
 const getAll = () => {
+  //select * from accounts
   return db('accounts')
 }
 
-const getById = id => {
-  return db('accounts')
-  .where('id', id)
-  .first()
+const getById = (id) => {
+  //select * from accounts where id = 1
+  return db('accounts').where('id', id).first()
 }
 
-const create = account => {
- return db('accounts')
- .create(account)
- .then(id => {
-   return getById(id)
- })
+const create = async (account) => {
+  // insert into accounts (name, budget) values ('foo', 1000)
+  const [ id ] = await db('accounts').insert(account)
+  return getById(id)
 }
 
-const updateById = (id, account) => {
-  return db('accounts')
-  .where('id', id)
-  .updateById(account)
+const updateById = async (id, account) => {
+  //update accounts set name = foo, budget = 1000 where id = 1
+  await db('accounts').where('id', id).update(account)
+  return getById(id)
 }
 
-const deleteById = id => {
-  return db('accounts')
-  .where('id', id)
-  .deleteById()
+const deleteById = async (id) => {
+  //delete from accounts where id = 1
+  return db('accounts').where('id', id).del()   
 }
 
 module.exports = {
