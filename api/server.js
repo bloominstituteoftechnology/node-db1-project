5f0c9d2e00a1d5138c9d37ee956a3ba89996d2f7
Alieze-Ali <71855821+Alieze-Ali@users.noreply.github.com>
2022-02-28 14:07:02 -0800

    global endpoint running

--- api/server.js
+++ api/server.js
@@ -4,4 +4,11 @@ const server = express();
 
 server.use(express.json());
 
+// Global Endpoint 
+server.use('*', (req, res) => {
+    res.status(404).json({
+        message: 'not found',
+    })
+})
+
 module.exports = server;
