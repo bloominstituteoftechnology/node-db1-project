a96406bfe66eae4b6479397d738a9b75f1efa57e
Amanda Burtness <amandaburtness@gmail.com>
2022-03-04 14:00:43 -0600

    mvp

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,21 +1,34 @@
+const db = require('../../data/db-config')
+
 const getAll = () => {
   // DO YOUR MAGIC
+  // select * from accounts
+  return db('accounts')
 }
 
 const getById = id => {
   // DO YOUR MAGIC
+  //  select * from accounts where id = id
+  return db('accounts').where('id', id).first()
 }
 
-const create = account => {
+const create = ({name, budget}) => {
   // DO YOUR MAGIC
+  const [id] = await db('accounts').insert({name, budget})
+  return getById(id)
 }
 
-const updateById = (id, account) => {
-  // DO YOUR MAGIC
+const updateById = (id, {account, budget}) => {
+  // DO YOUR MAGIC 
+  await db('accounts').where('id', id).update({account, budget})
+  return getById(id)
 }
 
 const deleteById = id => {
   // DO YOUR MAGIC
+  const deletedAccount = await getById(id)
+  await db('accounts').where('id', id).delete()
+  return deletedAccount
 }
 
 module.exports = {
